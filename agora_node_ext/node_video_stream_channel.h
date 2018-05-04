f44a95f05b7f0aa87546557c473d514c3d740325
wang yongli <wangyongli@agora.io>
2018-05-04 11:22:40 +0800

    Fix the crash issue if reinitialize video render transporter.

--- agora_node_ext/node_video_stream_channel.h
+++ agora_node_ext/node_video_stream_channel.h
@@ -31,20 +31,6 @@ namespace agora {
             ~NodeVideoStreamChannel();
 
             int deliverFrame(const agora::media::IVideoFrame& videoFrame, int rotation, bool mirrored);
-
-        private:
-            struct image_header_type {
-                uint8_t format;
-                uint8_t mirrored;
-                uint16_t width;
-                uint16_t height;
-                uint16_t left;
-                uint16_t top;
-                uint16_t right;
-                uint16_t bottom;
-                uint16_t rotation;
-                uint32_t timestamp;
-            };
         private:
             typedef std::vector<unsigned char> stream_buffer_type;
             std::unique_ptr<NodeRenderContext> m_context;
