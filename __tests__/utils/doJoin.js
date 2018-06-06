db61901806c9a2207859fa1fcc2ee25177cea8f2
Hao Yang <tillohao@gmail.com>
2018-06-06 17:34:29 +0800

    format

--- __tests__/utils/doJoin.js
+++ __tests__/utils/doJoin.js
@@ -1,22 +1,21 @@
-const generateRandomString = require('./index.js').generateRandomString
-const generateRandomNumber = require('./index.js').generateRandomNumber
+const generateRandomString = require('./index.js').generateRandomString;
+const generateRandomNumber = require('./index.js').generateRandomNumber;
 
-const doJoin = (rtcEngine) => {
+const doJoin = rtcEngine => {
   return new Promise((resolve, reject) => {
-    rtcEngine.setChannelProfile(1)
-    rtcEngine.setClientRole(2)
-    rtcEngine.setClientRole(0, 1)
+    rtcEngine.setChannelProfile(1);
+    rtcEngine.setClientRole(2);
+    rtcEngine.setClientRole(0, 1);
     rtcEngine.on('joinedchannel', () => {
-      resolve()
-    })
+      resolve();
+    });
     rtcEngine.on('error', err => {
-      reject(err)
-    })
-    // setTimeout(() => {
+      reject(err);
+    });
+    // SetTimeout(() => {
     //   reject('timeout')
     // }, 5000)
-    rtcEngine.joinChannel('', generateRandomString(10), '', generateRandomNumber(100000))
-  })
-
-}
-module.exports = doJoin
+    rtcEngine.joinChannel('', generateRandomString(10), '', generateRandomNumber(100000));
+  });
+};
+module.exports = doJoin;
