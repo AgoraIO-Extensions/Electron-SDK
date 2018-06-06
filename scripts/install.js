eeb48aebe9d9080eb18a19357d3d2a75842cce40
Hao Yang <tillohao@gmail.com>
2018-06-06 13:50:12 +0800

    auto build when do npm install || manual build by previous shell/cmd

--- scripts/install.js
+++ scripts/install.js
@@ -14,12 +14,12 @@ const install = () => {
   let platform = getPlatform()
   if(platform === 'mac') {
     shell.echo('Building AgoraRTC SDK for mac, this will cost a little time...')
-    if(shell.exec('node-gyp rebuild --target=1.8.3', {silent:true}).code !== 0) {
+    if(shell.exec('node-gyp rebuild', {silent:true}).code !== 0) {
       shell.echo('Building AgoraRTC SDK for mac failed.')
     }
   } else if (platform === 'win') {
     shell.echo('Building AgoraRTC SDK for windows 32bit, this will cost a little time...')
-    if (shell.exec('node-gyp rebuild --target=1.8.3 --arch=ia32 --msvs_version=2015').code !== 0) {
+    if (shell.exec('node-gyp rebuild --arch=ia32 --msvs_version=2015').code !== 0) {
       shell.echo('Building AgoraRTC SDK for window 32bit failed.')
     }
   } else {
