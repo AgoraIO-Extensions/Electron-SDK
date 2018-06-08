e1b7cb28685db3af9f238f376bda42ba6d98ab45
Hao Yang <tillohao@gmail.com>
2018-06-08 14:57:41 +0800

    solve conflicts

--- scripts/install.js
+++ scripts/install.js
@@ -22,7 +22,11 @@ const install = () => {
       'Building AgoraRTC SDK for windows 32bit, this will cost a little time...'
     );
     if (
+<<<<<<< HEAD
       shell.exec('node-gyp rebuild --arch=ia32 --msvs_version=2015 --target=1.8.3 --dist-url=https://atom.io/download/electron', { silent: true })
+=======
+      shell.exec('node-gyp rebuild --arch=ia32', { silent: true })
+>>>>>>> fixed script
         .code !== 0
     ) {
       shell.echo('Building AgoraRTC SDK for window 32bit failed.');
