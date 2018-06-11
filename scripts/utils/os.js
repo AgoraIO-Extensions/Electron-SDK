ee47eb1eb047b9c58cba0af2c747afc388e65601
Hao Yang <tillohao@gmail.com>
2018-06-11 16:54:55 +0800

    refactoring scripts / fixed a test case

---
+++ scripts/utils/os.js
@@ -0,0 +1,11 @@
+const getPlatform = () => {
+  if (process.platform === 'darwin') {
+    return 'mac';
+  }
+  if (process.platform === 'win32') {
+    return 'win';
+  }
+  return 'unsupported';
+};
+
+module.exports = getPlatform
