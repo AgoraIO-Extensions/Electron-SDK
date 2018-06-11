3598ee714a86fbb0921cb87eb69191ad09883577
menthays <tillohao@gmail.com>
2018-06-11 19:23:55 +0800

    fixed runtime when build on windows

--- scripts/gen.js
+++ scripts/gen.js
@@ -9,18 +9,18 @@ const generate = () => {
   let platform = getPlatform();
   if (platform === 'mac') {
     shell.echo('Generating project file for mac, this will cost a little time...');
-    if (shell.exec('node-gyp configure -- -f xcode', { silent: true }).code !== 0) {
-      shell.echo('Finished. Find it under "/build".');
+    if (shell.exec('node-gyp configure -- -f xcode --target=1.8.3 --dist-url=https://atom.io/download/electron', { silent: true }).code !== 0) {
+      shell.echo('Failed to generate project file".');
     }
   } else if (platform === 'win') {
     shell.echo(
       'Generating project file for win32, this will cost a little time...'
     );
     if (
-      shell.exec('node-gyp configure --arch=ia32 --debug', { silent: true })
+      shell.exec('node-gyp configure --arch=ia32 --debug --target=1.8.3 --dist-url=https://atom.io/download/electron', { silent: true })
         .code !== 0
     ) {
-      shell.echo('Finshed. Find it under "/build".');
+      shell.echo('Failed to generate project file".');
     }
   } else {
     shell.echo('Sorry, this sdk only provide win32 and mac version.');
