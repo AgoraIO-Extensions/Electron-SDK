8b15f68959ce43d1fed513a39c5c377861da9619
menthays <tillohao@gmail.com>
2018-06-11 19:23:55 +0800

    fixed runtime when build on windows

--- scripts/install.js
+++ scripts/install.js
@@ -1,11 +1,14 @@
 const shell = require('shelljs');
 const getPlatform = require('./utils/os')
+const argv = require('optimist').argv;
 
 const install = () => {
   let platform = getPlatform();
+  let electronArgs = argv.runtime === 'electron' ? 
+    ' --target=1.8.3 --dist-url=https://atom.io/download/electron' : ''
   if (platform === 'mac') {
     shell.echo('Building AgoraRTC SDK for mac, this will cost a little time...');
-    if (shell.exec('node-gyp rebuild', { silent: true }).code !== 0) {
+    if (shell.exec('node-gyp rebuild'+electronArgs, { silent: true }).code !== 0) {
       shell.echo('Building AgoraRTC SDK for mac failed.');
     }
   } else if (platform === 'win') {
@@ -13,7 +16,7 @@ const install = () => {
       'Building AgoraRTC SDK for windows 32bit, this will cost a little time...'
     );
     if (
-      shell.exec('node-gyp rebuild --arch=ia32', { silent: true })
+      shell.exec('node-gyp rebuild --arch=ia32'+electronArgs, { silent: true })
         .code !== 0
     ) {
       shell.echo('Building AgoraRTC SDK for window 32bit failed.');
