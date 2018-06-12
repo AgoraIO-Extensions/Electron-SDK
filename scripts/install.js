0b0909669794776d237cf55dd1091d59f24dedfb
Hao Yang <tillohao@gmail.com>
2018-06-12 11:15:06 +0800

    update readme and terminal ui, rdy for publish

--- scripts/install.js
+++ scripts/install.js
@@ -1,28 +1,33 @@
 const shell = require('shelljs');
 const getPlatform = require('./utils/os')
 const argv = require('optimist').argv;
+const chalk = require('chalk');
 
 const install = () => {
   let platform = getPlatform();
   let electronArgs = argv.runtime === 'electron' ? 
     ' --target=1.8.3 --dist-url=https://atom.io/download/electron' : ''
   if (platform === 'mac') {
-    shell.echo('Building AgoraRTC SDK for mac, this will cost a little time...');
+    shell.echo(chalk.blue('Building AgoraRTC SDK for mac, this will cost a little time...'));
     if (shell.exec('node-gyp rebuild'+electronArgs, { silent: true }).code !== 0) {
-      shell.echo('Building AgoraRTC SDK for mac failed.');
+      shell.echo(chalk.red('Building AgoraRTC SDK for mac failed.'));
+    } else {
+      shell.echo(chalk.green('Building finished successfully'))
     }
   } else if (platform === 'win') {
     shell.echo(
-      'Building AgoraRTC SDK for windows 32bit, this will cost a little time...'
+      chalk.blue('Building AgoraRTC SDK for windows 32bit, this will cost a little time...')
     );
     if (
       shell.exec('node-gyp rebuild --arch=ia32'+electronArgs, { silent: true })
         .code !== 0
     ) {
-      shell.echo('Building AgoraRTC SDK for window 32bit failed.');
+      shell.echo(chalk.red('Building AgoraRTC SDK for window 32bit failed.'));
+    } else {
+      shell.echo(chalk.green('Building finished successfully'))
     }
   } else {
-    shell.echo('Sorry, this sdk only provide win32 and mac version.');
+    shell(chalk.yellowBright('Sorry, this sdk only provide win32 and mac version.'));
     shell.exit(1);
   }
 };
