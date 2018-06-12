7707a311bbbe815448fd328424846510f1407d6e
Hao Yang <tillohao@gmail.com>
2018-06-12 13:15:35 +0800

    format code and update pkg.json info

--- scripts/gen.js
+++ scripts/gen.js
@@ -3,29 +3,38 @@
  */
 
 const shell = require('shelljs');
-const getPlatform = require('./utils/os')
+const getPlatform = require('./utils/os');
 const chalk = require('chalk');
 
 const generate = () => {
   let platform = getPlatform();
   if (platform === 'mac') {
-    shell.echo(chalk.blue('Generating project file for mac, this will cost a little time...'));
-    if (shell.exec('node-gyp configure --target=1.8.3 --dist-url=https://atom.io/download/electron -- -f xcode ', { silent: true }).code !== 0) {
+    shell.echo(
+      chalk.blue('Generating project file for mac, this will cost a little time...')
+    );
+    if (
+      shell.exec(
+        'node-gyp configure --target=1.8.3 --dist-url=https://atom.io/download/electron -- -f xcode ',
+        { silent: true }
+      ).code !== 0
+    ) {
       shell.echo(chalk.red('Failed to generate project file.'));
     } else {
-      shell.echo(chalk.green('Generating finished successfully'))
+      shell.echo(chalk.green('Generating finished successfully'));
     }
   } else if (platform === 'win') {
     shell.echo(
       chalk.blue('Generating project file for win32, this will cost a little time...')
     );
     if (
-      shell.exec('node-gyp configure --target=1.8.3 --dist-url=https://atom.io/download/electron --arch=ia32 --debug', { silent: true })
-        .code !== 0
+      shell.exec(
+        'node-gyp configure --target=1.8.3 --dist-url=https://atom.io/download/electron --arch=ia32 --debug',
+        { silent: true }
+      ).code !== 0
     ) {
       shell.echo(chalk.red('Failed to generate project file".'));
     } else {
-      shell.echo(chalk.green('Generating finished successfully'))
+      shell.echo(chalk.green('Generating finished successfully'));
     }
   } else {
     shell.echo(chalk.yellowBright('Sorry, this sdk only provide win32 and mac version.'));
@@ -33,4 +42,4 @@ const generate = () => {
   }
 };
 
-generate()
+generate();
--- scripts/install.js
+++ scripts/install.js
@@ -1,30 +1,36 @@
 const shell = require('shelljs');
-const getPlatform = require('./utils/os')
+const getPlatform = require('./utils/os');
 const argv = require('optimist').argv;
 const chalk = require('chalk');
 
 const install = () => {
   let platform = getPlatform();
-  let electronArgs = argv.runtime === 'electron' ? 
-    ' --target=1.8.3 --dist-url=https://atom.io/download/electron' : ''
+  let electronArgs =
+    argv.runtime === 'electron'
+      ? ' --target=1.8.3 --dist-url=https://atom.io/download/electron'
+      : '';
   if (platform === 'mac') {
-    shell.echo(chalk.blue('Building AgoraRTC SDK for mac, this will cost a little time...'));
-    if (shell.exec('node-gyp rebuild'+electronArgs, { silent: true }).code !== 0) {
+    shell.echo(
+      chalk.blue('Building AgoraRTC SDK for mac, this will cost a little time...')
+    );
+    if (shell.exec('node-gyp rebuild' + electronArgs, { silent: true }).code !== 0) {
       shell.echo(chalk.red('Building AgoraRTC SDK for mac failed.'));
     } else {
-      shell.echo(chalk.green('Building finished successfully'))
+      shell.echo(chalk.green('Building finished successfully'));
     }
   } else if (platform === 'win') {
     shell.echo(
-      chalk.blue('Building AgoraRTC SDK for windows 32bit, this will cost a little time...')
+      chalk.blue(
+        'Building AgoraRTC SDK for windows 32bit, this will cost a little time...'
+      )
     );
     if (
-      shell.exec('node-gyp rebuild --arch=ia32'+electronArgs, { silent: true })
-        .code !== 0
+      shell.exec('node-gyp rebuild --arch=ia32' + electronArgs, { silent: true }).code !==
+      0
     ) {
       shell.echo(chalk.red('Building AgoraRTC SDK for window 32bit failed.'));
     } else {
-      shell.echo(chalk.green('Building finished successfully'))
+      shell.echo(chalk.green('Building finished successfully'));
     }
   } else {
     shell(chalk.yellowBright('Sorry, this sdk only provide win32 and mac version.'));
