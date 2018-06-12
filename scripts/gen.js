d2e777e0b7ba78197a9b643fda463afdcc0d67c2
Hao Yang <tillohao@gmail.com>
2018-06-12 11:15:06 +0800

    update readme and terminal ui, rdy for publish

--- scripts/gen.js
+++ scripts/gen.js
@@ -4,26 +4,31 @@
 
 const shell = require('shelljs');
 const getPlatform = require('./utils/os')
+const chalk = require('chalk');
 
 const generate = () => {
   let platform = getPlatform();
   if (platform === 'mac') {
-    shell.echo('Generating project file for mac, this will cost a little time...');
-    if (shell.exec('node-gyp configure -- -f xcode --target=1.8.3 --dist-url=https://atom.io/download/electron', { silent: true }).code !== 0) {
-      shell.echo('Failed to generate project file".');
+    shell.echo(chalk.blue('Generating project file for mac, this will cost a little time...'));
+    if (shell.exec('node-gyp configure --target=1.8.3 --dist-url=https://atom.io/download/electron -- -f xcode ', { silent: true }).code !== 0) {
+      shell.echo(chalk.red('Failed to generate project file.'));
+    } else {
+      shell.echo(chalk.green('Generating finished successfully'))
     }
   } else if (platform === 'win') {
     shell.echo(
-      'Generating project file for win32, this will cost a little time...'
+      chalk.blue('Generating project file for win32, this will cost a little time...')
     );
     if (
-      shell.exec('node-gyp configure --arch=ia32 --debug --target=1.8.3 --dist-url=https://atom.io/download/electron', { silent: true })
+      shell.exec('node-gyp configure --target=1.8.3 --dist-url=https://atom.io/download/electron --arch=ia32 --debug', { silent: true })
         .code !== 0
     ) {
-      shell.echo('Failed to generate project file".');
+      shell.echo(chalk.red('Failed to generate project file".'));
+    } else {
+      shell.echo(chalk.green('Generating finished successfully'))
     }
   } else {
-    shell.echo('Sorry, this sdk only provide win32 and mac version.');
+    shell.echo(chalk.yellowBright('Sorry, this sdk only provide win32 and mac version.'));
     shell.exit(1);
   }
 };
