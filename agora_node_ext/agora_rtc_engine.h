aa3db141a14d3805bf00b79f7a8af7c634cb91a9
张乾泽 <qianze.zhang@hotmail.com>
2018-07-30 11:57:01 +0800

    adding enable dual stream for video source
    Former-commit-id: 9e461bd82776ce1f11504e931734e5357eb975ae

--- agora_node_ext/agora_rtc_engine.h
+++ agora_node_ext/agora_rtc_engine.h
@@ -206,6 +206,7 @@ namespace agora {
             NAPI_API(videoSourceStartPreview);
             NAPI_API(videoSourceStopPreview);
             NAPI_API(videoSourceEnableWebSdkInteroperability);
+            NAPI_API(videoSourceEnableDualStreamMode);
             NAPI_API(videoSourceSetParameter);
 
             /*
