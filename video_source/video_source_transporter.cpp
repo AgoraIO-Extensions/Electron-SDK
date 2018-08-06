1ec34060dcac5056f44122c6d63fe13d19168a02
张乾泽 <qianze.zhang@hotmail.com>
2018-08-06 15:14:30 +0800

    fixed screen resolution on mac issue
    Former-commit-id: 10a5ecb253cc95867d8ada7ed7358afb0d7c9fcd

--- video_source/video_source_transporter.cpp
+++ video_source/video_source_transporter.cpp
@@ -229,95 +229,95 @@ void AgoraVideoSourceTransporter::getVideoSize(VIDEO_PROFILE_TYPE profile, int& 
 		height = 2160;
 		break;
 #elif defined(__APPLE__)
-    case VIDEO_PROFILE_PORTRAIT_120P:         // 160x120   15   65
+    case VIDEO_PROFILE_LANDSCAPE_120P:         // 160x120   15   65
         width = 160;
         height = 120;
         break;
-    case VIDEO_PROFILE_PORTRAIT_120P_3:      // 120x120   15   50
+    case VIDEO_PROFILE_LANDSCAPE_120P_3:      // 120x120   15   50
         width = 120;
         height = 120;
         break;
-    case VIDEO_PROFILE_PORTRAIT_180P:        // 320x180   15   140
+    case VIDEO_PROFILE_LANDSCAPE_180P:        // 320x180   15   140
         width = 320;
         height = 180;
         break;
-    case VIDEO_PROFILE_PORTRAIT_180P_3:     // 180x180   15   100
+    case VIDEO_PROFILE_LANDSCAPE_180P_3:     // 180x180   15   100
         width = 180;
         height = 180;
         break;
-    case VIDEO_PROFILE_PORTRAIT_180P_4:     // 240x180   15   120
+    case VIDEO_PROFILE_LANDSCAPE_180P_4:     // 240x180   15   120
         width = 240;
         height = 180;
         break;
-    case VIDEO_PROFILE_PORTRAIT_240P:       // 320x240   15   200
+    case VIDEO_PROFILE_LANDSCAPE_240P:       // 320x240   15   200
         width = 320;
         height = 240;
         break;
-    case VIDEO_PROFILE_PORTRAIT_240P_3:      // 240x240   15   140
+    case VIDEO_PROFILE_LANDSCAPE_240P_3:      // 240x240   15   140
         width = 240;
         height = 240;
         break;
-    case VIDEO_PROFILE_PORTRAIT_240P_4:     // 424x240   15   220
+    case VIDEO_PROFILE_LANDSCAPE_240P_4:     // 424x240   15   220
         width = 424;
         height = 240;
         break;
-    case VIDEO_PROFILE_PORTRAIT_360P_3:    // 360x360   15   260
-    case VIDEO_PROFILE_PORTRAIT_360P_6:    // 360x360   30   400
+    case VIDEO_PROFILE_LANDSCAPE_360P_3:    // 360x360   15   260
+    case VIDEO_PROFILE_LANDSCAPE_360P_6:    // 360x360   30   400
         width = 360;
         height = 360;
         break;
-    case VIDEO_PROFILE_PORTRAIT_360P_4:    // 640x360   30   600
-    case VIDEO_PROFILE_PORTRAIT_360P_9:    // 640x360   15   800
-    case VIDEO_PROFILE_PORTRAIT_360P_10:   // 640x360   24   800
-    case VIDEO_PROFILE_PORTRAIT_360P_11:   // 640x360   24   1000
+    case VIDEO_PROFILE_LANDSCAPE_360P_4:    // 640x360   30   600
+    case VIDEO_PROFILE_LANDSCAPE_360P_9:    // 640x360   15   800
+    case VIDEO_PROFILE_LANDSCAPE_360P_10:   // 640x360   24   800
+    case VIDEO_PROFILE_LANDSCAPE_360P_11:   // 640x360   24   1000
         width = 640;
         height = 360;
         break;
-    case VIDEO_PROFILE_PORTRAIT_360P_7:    // 480x360   15   320
-    case VIDEO_PROFILE_PORTRAIT_360P_8:    // 480x360   30   490
+    case VIDEO_PROFILE_LANDSCAPE_360P_7:    // 480x360   15   320
+    case VIDEO_PROFILE_LANDSCAPE_360P_8:    // 480x360   30   490
         width = 480;
         height = 360;
         break;
 
-    case VIDEO_PROFILE_PORTRAIT_480P:      // 640x480   15   500
-    case VIDEO_PROFILE_PORTRAIT_480P_3:    // 480x480   15   400
-    case VIDEO_PROFILE_PORTRAIT_480P_4:    // 640x480   30   750
-    case VIDEO_PROFILE_PORTRAIT_480P_10:   // 640x480   10   400
+    case VIDEO_PROFILE_LANDSCAPE_480P:      // 640x480   15   500
+    case VIDEO_PROFILE_LANDSCAPE_480P_3:    // 480x480   15   400
+    case VIDEO_PROFILE_LANDSCAPE_480P_4:    // 640x480   30   750
+    case VIDEO_PROFILE_LANDSCAPE_480P_10:   // 640x480   10   400
         width = 640;
         height = 480;
         break;
-    case VIDEO_PROFILE_PORTRAIT_480P_6:    // 480x480   30   600
+    case VIDEO_PROFILE_LANDSCAPE_480P_6:    // 480x480   30   600
         width = 480;
         height = 480;
         break;
-    case VIDEO_PROFILE_PORTRAIT_480P_8:    // 848x480   15   610
-    case VIDEO_PROFILE_PORTRAIT_480P_9:    // 848x480   30   930
+    case VIDEO_PROFILE_LANDSCAPE_480P_8:    // 848x480   15   610
+    case VIDEO_PROFILE_LANDSCAPE_480P_9:    // 848x480   30   930
         width = 848;
         height = 480;
         break;
-    case VIDEO_PROFILE_PORTRAIT_720P:      // 1280x720  15   1130
-    case VIDEO_PROFILE_PORTRAIT_720P_3:    // 1280x720  30   1710
+    case VIDEO_PROFILE_LANDSCAPE_720P:      // 1280x720  15   1130
+    case VIDEO_PROFILE_LANDSCAPE_720P_3:    // 1280x720  30   1710
         width = 1280;
         height = 720;
         break;
-    case VIDEO_PROFILE_PORTRAIT_720P_5:    // 960x720   15   910
-    case VIDEO_PROFILE_PORTRAIT_720P_6:    // 960x720   30   1380
+    case VIDEO_PROFILE_LANDSCAPE_720P_5:    // 960x720   15   910
+    case VIDEO_PROFILE_LANDSCAPE_720P_6:    // 960x720   30   1380
         width = 960;
         height = 720;
         break;
-    case VIDEO_PROFILE_PORTRAIT_1080P:     // 1920x1080 15   2080
-    case VIDEO_PROFILE_PORTRAIT_1080P_3:   // 1920x1080 30   3150
-    case VIDEO_PROFILE_PORTRAIT_1080P_5:   // 1920x1080 60   4780
+    case VIDEO_PROFILE_LANDSCAPE_1080P:     // 1920x1080 15   2080
+    case VIDEO_PROFILE_LANDSCAPE_1080P_3:   // 1920x1080 30   3150
+    case VIDEO_PROFILE_LANDSCAPE_1080P_5:   // 1920x1080 60   4780
         width = 1920;
         height = 1080;
         break;
-    case VIDEO_PROFILE_PORTRAIT_1440P:     // 2560x1440 30   4850
-    case VIDEO_PROFILE_PORTRAIT_1440P_2:   // 2560x1440 60   7350
+    case VIDEO_PROFILE_LANDSCAPE_1440P:     // 2560x1440 30   4850
+    case VIDEO_PROFILE_LANDSCAPE_1440P_2:   // 2560x1440 60   7350
         width = 2560;
         height = 1440;
         break;
-    case VIDEO_PROFILE_PORTRAIT_4K:     // 2560x1440 30   4850
-    case VIDEO_PROFILE_PORTRAIT_4K_3:   // 2560x1440 60   7350
+    case VIDEO_PROFILE_LANDSCAPE_4K:     // 2560x1440 30   4850
+    case VIDEO_PROFILE_LANDSCAPE_4K_3:   // 2560x1440 60   7350
         width = 3840;
         height = 2160;
         break;
