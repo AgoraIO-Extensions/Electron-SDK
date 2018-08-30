45cdf4d6abfc4e85e48ba49bdc2e94d4713395a4
Hao Yang <tillohao@gmail.com>
2018-08-30 19:17:13 +0800

    add example and scripts:example

---
+++ example/src/renderer/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.css';
+import App from './App';
+import './index.sass'
+
+ReactDOM.render(<App />, document.getElementById('app'));
