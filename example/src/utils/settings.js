39b11b78633c2782c8a3ae43434f549212bab576
Hao Yang <tillohao@gmail.com>
2018-08-30 19:17:13 +0800

    add example and scripts:example

---
+++ example/src/utils/settings.js
@@ -0,0 +1,82 @@
+export const APP_ID = 'aab8b8f5a8cd4469a63042fcfafe7063'
+
+export const videoProfileList = [
+  {
+    value: 0,
+    label: '160x120	15fps	65kbps'
+  },
+  {
+    value: 20,
+    label: '320x240	15fps	200kbps'
+  },
+  {
+    value: 30,
+    label: '640x360	15fps	400kbps'
+  },
+  {
+    value: 43,
+    label: '640x480	30fps	750kbps'
+  },
+  {
+    value: 50,
+    label: '1280x720 15fps 1130kbps'
+  },
+  {
+    value: 60,
+    label: '1920x1080 15fps 2080kbps'
+  }
+]
+
+export const audioProfileList = [
+  {
+    value: 0,
+    label: 'AUDIO_PROFILE_DEFAULT'
+  },
+  {
+    value: 1,
+    label: 'AUDIO_PROFILE_SPEECH_STANDARD'
+  },
+  {
+    value: 2,
+    label: 'AUDIO_PROFILE_MUSIC_STANDARD'
+  },
+  {
+    value: 3,
+    label: 'AUDIO_PROFILE_MUSIC_STANDARD_STEREO'
+  },
+  {
+    value: 4,
+    label: 'AUDIO_PROFILE_MUSIC_HIGH_QUALITY'
+  },
+  {
+    value: 5,
+    label: 'AUDIO_PROFILE_MUSIC_HIGH_QUALITY_STEREO'
+  } 
+]
+
+export const audioScenarioList = [
+  {
+    value: 0,
+    label: 'AUDIO_PROFILE_DEFAULT'
+  },
+  {
+    value: 1,
+    label: 'AUDIO_SCENARIO_CHATROOM_ENTERTAINMENT'
+  },
+  {
+    value: 2,
+    label: 'AUDIO_SCENARIO_EDUCATION'
+  },
+  {
+    value: 3,
+    label: 'AUDIO_SCENARIO_GAME_STREAMING'
+  },
+  {
+    value: 4,
+   label: 'AUDIO_SCENARIO_SHOWROOM'
+  },
+  {
+    value: 5,
+    label: 'AUDIO_SCENARIO_CHATROOM_GAMING'
+  } 
+]
