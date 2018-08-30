84ec8ce9d48310a04f5242cac24a575e2569e7f4
Hao Yang <tillohao@gmail.com>
2018-08-30 19:27:46 +0800

    remove useless lib (sass related and sdk on npm)

--- example/webpack.renderer.additions.js
+++ example/webpack.renderer.additions.js
@@ -1,20 +1,3 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s[a|c]ss$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      }
-    ]
-  }
+
 }
