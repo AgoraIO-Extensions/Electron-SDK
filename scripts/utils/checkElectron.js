aeabb11c6bc083f7e682c914a298ba423f3dad7f
Hao Yang <tillohao@gmail.com>
2018-11-19 14:42:52 +0800

    fixed when electron were in dev dep

--- scripts/utils/checkElectron.js
+++ scripts/utils/checkElectron.js
@@ -1,6 +1,6 @@
 const semver = require('semver');
 
-const electronDepVersion = process.env.npm_package_dependencies_electron;
+const electronDepVersion = process.env.npm_package_dependencies_electron || process.env.npm_package_devDependencies_electron;
 
 const getElectronVersion = function() {
   if (!electronDepVersion) {
