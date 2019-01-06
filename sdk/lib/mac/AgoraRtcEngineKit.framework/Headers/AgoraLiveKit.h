252b966aa36bf2a8d728507b789a61459cdbbcd1
张乾泽 <qianze.zhang@hotmail.com>
2019-01-06 15:18:35 +0800

    sdk update

--- sdk/lib/mac/AgoraRtcEngineKit.framework/Headers/AgoraLiveKit.h
+++ sdk/lib/mac/AgoraRtcEngineKit.framework/Headers/AgoraLiveKit.h
@@ -12,149 +12,194 @@
 #import "AgoraLiveSubscriber.h"
 #import "AgoraRtcEngineKit.h"
 
+/** A class for enabling or disabling the video.
+ */
 __attribute__((visibility("default"))) @interface AgoraLiveChannelConfig: NSObject
 @property (assign, nonatomic) BOOL videoEnabled;
 
 +(AgoraLiveChannelConfig *_Nonnull) defaultConfig;
 @end
 
+/** The base class for managing a live broadcast.
+
+ AgoraLiveKit manages the channel and all actions apart from publishing and subscribing.
+ */
 @class AgoraLiveKit;
 
+/** Protocol providing the AgoraLiveKit class with callbacks.
+ */
 @protocol AgoraLiveDelegate <NSObject>
 @optional
 
-/**
- *  The warning occurred in SDK. The APP could igonre the warning, and the SDK could try to resume automically.
- *
- *  @param kit         The live kit
- *  @param warningCode The warning code
+
+/** A warning occurred during SDK runtime.
+
+ The application can ignore the warning, and the SDK tries to resume automatically.
+
+ @param kit         AgoraLiveKit
+ @param warningCode AgoraWarningCode
  */
 - (void)liveKit:(AgoraLiveKit *_Nonnull)kit didOccurWarning:(AgoraWarningCode)warningCode;
 
-/**
- *  The error occurred in SDK. The SDK couldn't resume to normal state, and the app need to handle it.
- *
- *  @param kit       The live kit
- *  @param errorCode The error code
+/** An error occurred during SDK runtime.
+
+ The SDK cannot resume a normal state, and the application needs to handle it.
+
+ @param kit       AgoraLiveKit
+ @param errorCode AgoraErrorCode
  */
 - (void)liveKit:(AgoraLiveKit *_Nonnull)kit didOccurError:(AgoraErrorCode)errorCode;
 
-/**
- *  Event of the user joined the channel.
- *
- *  @param kit     The live kit
- *  @param channel The channel name
- *  @param uid     The local user id
- *  @param elapsed The elapsed time (ms) from session beginning
- */
+/** A user joined the channel.
+
+ @param kit     AgoraLiveKit
+ @param channel Channel name
+ @param uid     Local user ID
+ @param elapsed Time elapsed (ms) from calling [joinChannelByToken]([AgoraRtcEngineKit joinChannelByToken:channelId:info:uid:joinSuccess:]) until this callback is triggered.
+  */
 - (void)liveKit:(AgoraLiveKit *_Nonnull)kit didJoinChannel:(NSString *_Nonnull)channel withUid:(NSUInteger)uid elapsed:(NSInteger) elapsed;
 
-/**
- *  The statistics of the call when leave channel
- *
- *  @param kit    The live kit
+/** The statistics of the call when leaving a channel
+
+ @param kit    AgoraLiveKit
  */
 - (void)liveKitDidLeaveChannel:(AgoraLiveKit *_Nonnull)kit;
 
-/**
- *  Event of the user rejoined the channel
- *
- *  @param kit     The live kit
- *  @param channel The channel name
- *  @param uid     The user id
- *  @param elapsed The elapsed time (ms) from session beginning
+/** A user rejoined the channel
+
+ @param kit     AgoraLiveKit
+ @param channel Channel name.
+ @param uid     User ID.
+ @param elapsed Time elapsed (ms) from calling [joinChannelByToken]([AgoraRtcEngineKit joinChannelByToken:channelId:info:uid:joinSuccess:]) until this callback is triggered.
  */
 - (void)liveKit:(AgoraLiveKit *_Nonnull)kit didRejoinChannel:(NSString *_Nonnull)channel withUid:(NSUInteger)uid elapsed:(NSInteger) elapsed;
 
-/**
- * when token is enabled, and specified token is invalid or expired, this function will be called.
- * APP should generate a new token and call renewToken() to refresh the key.
- * NOTE: to be compatible with previous version, ERR_TOKEN_EXPIRED and ERR_INVALID_TOKEN are also reported via onError() callback.
- * You should move renew of token logic into this callback.
- *  @param kit The live kit
+/** Generates a new Token.
+
+ When the Token is enabled, and the specified Token is invalid or has expired, this function will be called.
+ The app should generate a new Token and call renewToken to refresh the key.
+
+ Note: AgoraErrorCodeTokenExpired = -109 and AgoraErrorCodeInvalidToken = -110 are also reported through the [didOccurError](liveKit:didOccurError:) callback. Move the renew token logic into this callback.
+
+ @param kit AgoraLiveKit
  */
 - (void)liveKitRequestToken:(AgoraLiveKit *_Nonnull)kit;
 
+/**
+ The Token will expire within 30 seconds.
+
+ If the Token you specified when calling [joinChannelByToken]([AgoraRtcEngineKit joinChannelByToken:channelId:info:uid:joinSuccess:]) expires, you will become offline. This callback is triggered 30 seconds before the Token expires to remind the app to renew the Token.
+
+ Upon receiving this callback, the user needs to generate a new Token on your server and call renewToken to pass the new Token on to the SDK.
+
+
+ @param kit AgoraLiveKit
+ @param token The Token that will expire in 30 seconds.
+ */
+- (void)liveKit:(AgoraLiveKit * _Nonnull)kit tokenPrivilegeWillExpire:(NSString * _Nonnull)token;
+
 // statistics
 
-/**
- *  Statistics of rtc live kit status. Updated every two seconds.
- *
- *  @param kit    The live kit
- *  @param stats  The statistics of rtc status, including duration, sent bytes and received bytes
+/** The statistics of the RTC AgoraLiveKit status every two seconds.
+
+ @param kit    AgoraLiveKit
+ @param stats  Statistics of the RTC status, including the duration, sent bytes, and received bytes
  */
 - (void)liveKit:(AgoraLiveKit *_Nonnull)kit reportLiveStats:(AgoraChannelStats *_Nonnull)stats;
 // network
 
-/**
- *  Event of disconnected with server. This event is reported at the moment SDK loses connection with server.
- *  In the mean time SDK automatically tries to reconnect with the server until APP calls leaveChannel.
- *
- *  @param kit    The live kit
+/** The connection between the SDK and the server has been interrupted.
+
+This callback is triggered when the SDK losses connection with the server.
+
+Once the connection is lost, the SDK attempts to reconnect until the
+   application calls [leaveChannel]([AgoraLiveKit leaveChannel]).
+
+ @param kit    AgoraLiveKit
  */
 - (void)liveKitConnectionDidInterrupted:(AgoraLiveKit *_Nonnull)kit;
 
-/**
- *  Event of loss connection with server. This event is reported after the connection is interrupted and exceed the retry period (10 seconds by default).
- *  In the mean time SDK automatically tries to reconnect with the server until APP calls leaveChannel.
- *
- *  @param kit    The live kit
+/** The connection to the server is lost.
+
+ This event is reported after the connection to the server is interrupted and the reconnection time has exceeded (10 seconds by default).
+ The SDK tries to automatically reconnect with the server until the app calls [leaveChannel]([AgoraLiveKit leaveChannel]).
+
+@param kit    AgoraLiveKit
  */
 - (void)liveKitConnectionDidLost:(AgoraLiveKit *_Nonnull)kit;
 
-/**
- *  The network quality of local user.
- *
- *  @param kit     The live kit
- *  @param uid     The id of user
- *  @param txQuality The sending network quality
- *  @param rxQuality The receiving network quality
+/** The network quality of the local user.
+
+ @param kit     AgoraLiveKit
+ @param uid     User ID
+ @param txQuality Network transmission quality: AgoraNetworkQuality
+ @param rxQuality Network receiving quality: AgoraNetworkQuality
  */
 - (void)liveKit:(AgoraLiveKit *_Nonnull)kit networkQuality:(NSUInteger)uid txQuality:(AgoraNetworkQuality)txQuality rxQuality:(AgoraNetworkQuality)rxQuality;
 @end
 
+/**
+ AgoraLiveKit is the base class for managing a live broadcast. AgoraLiveKit manages the channel and all actions apart from publishing and subscribing.
+ */
 __attribute__((visibility("default"))) @interface AgoraLiveKit : NSObject
 
+/**
+ Protocol providing the AgoraLiveKit class with callbacks.
+ */
 @property (weak, nonatomic) _Nullable id<AgoraLiveDelegate> delegate;
 
-/**
-*  Get the version of Agora SDK.
-*
-*  @return string, sdk version
+/** Returns the version of the Agora SDK.
+
+@return string, SDK version
 */
 + (NSString *_Nonnull)getSdkVersion;
 
-/**
- *  Get the native handler of sdk Engine
+/** Returns the native handler of the SDK engine
  */
 - (AgoraRtcEngineKit *_Nonnull)getRtcEngineKit;
 
-/**
- *  Initializes the AgoraLiveKit object.
- *
- *  @param appId The appId is issued to the application developers by Agora.
- *
- *  @return an object of AgoraLiveKit class
+/** Initializes the AgoraLiveKit object.
+
+ @param appId The appId is issued to the application developers by Agora.
+
+ @return an object of AgoraLiveKit class
  */
 + (instancetype _Nonnull)sharedLiveKitWithAppId:(NSString *_Nonnull)appId;
 
-+ (void)destroy;
-
-/** BEGIN OF COMMON METHODS */
 
 /**
- *  Create an open UDP socket to the AgoraLiveKit cloud service to join a channel.
-    Users in the same channel can talk to each other with same vendor key.
-    Users using different vendor keys cannot call each other.
-    The method is asynchronous.
- *
- *  @param channelId       Joining in the same channel indicates those clients have entered in one room.
- *  @param token        token generated by APP using sign certificate.
- *  @param channelConfig     configration of channel
- *  @param uid               Optional, this argument is the unique ID for each member in one channel.
-                             If set to 0, the SDK automatically allocates an ID, and the id could be gotten in didJoinChannel delegate.
- *
- *  @return 0 when executed successfully, and return negative value when failed.
+ Destroys the engine instance.
+
+ This method releases all the resources used by the Agora SDK.  This is useful for applications occasionally making voice calls, to free up resources for other operations when not making calls.
+
+ Once the application has called destroy() to destroy the created RtcEngine instance,
+ no other methods in the SDK can be used and no callbacks occur.
+
+ To start communications again, initialize [sharedEngineWithappId](sharedEngineWithAppId:delegate:) to establish a new AgoraRtcEngineKit instance.
+
+ Note: This method is called synchronously. The result returns after the IRtcEngine
+ object resources are released. The app should not call this interface in the
+ callback generated by the SDK, otherwise the SDK must wait for the callback
+ to return before it can reclaim the related object resources, causing a deadlock.
+
+ */
++ (void)destroy;
+
+/** Core Methods */
+
+/**  Allows a user to join a channel.
+
+ This method creates an open UDP socket to the AgoraLiveKit cloud service to join a channel.
+ Users in the same channel can talk to each other using the same App ID. This method is asynchronous.
+
+
+ @param token A Token generated by the app through signature certification.
+ @param channelId Channel ID.  Joining a channel with the same channel ID means that users are joining the same room.
+ The channel ID supported scope: a-z, A-Z, 0-9, space, !#$%&()+-:;&le;.,>?@[]^_{|}~
+ @param channelConfig AgoraLiveChannelConfig
+ @param uid Optional: ID of each user in the channel that is unique. If it is set as 0, the SDK automatically assigns an ID, which can be found in the [didJoinChannel]([AgoraLiveDelegate liveKit:didJoinChannel:withUid:elapsed:]) delegate.
+
+ @return * 0: Success. * <0: Failure.
  */
 - (int)joinChannelByToken:(NSString *_Nullable)token
                channelId:(NSString *_Nonnull)channelId
@@ -162,36 +207,36 @@ __attribute__((visibility("default"))) @interface AgoraLiveKit : NSObject
                uid:(NSUInteger)uid;
 
 /**
- *  lets the user leave a channel, i.e., hanging up or exiting a call.
-    After joining a channel, the user must call the leaveChannel method to end the call before joining another one.
-    It is synchronous, i.e., it only returns until the call ends and all resources are released.
- *
- *  @return 0 if executed successfully, or return negative value if failed.
+ Allows a user to leave a channel, such as hanging up or exiting a call.
+
+ @return * 0: Success. * <0: Failure.
  */
 - (int)leaveChannel;
 
 
-/**
- *  Renew token, refresh the new key into agora engine. APP should call this API when SDK reports error ERR_TOKEN_EXPIRED.
- *
- *  @param token new token
- *
- *  @return 0 when executed successfully. return negative value if failed.
+/** Updates the Token.
+
+ The key expires after a certain period of time once the Token scheme is enabled. When the [didOccurError]([AgoraLiveDelegate liveKit:didOccurError:]) callback reports the error ERR_TOKEN_EXPIRED(109), the application should retrieve a new key and then call this method to renew it. Failure to do so will result in the SDK disconnecting from the server.
+
+ @param token New Token.
+
+ @return * 0: Success. * <0: Failure.
  */
 - (int)renewToken:(NSString*_Nonnull)token;
 
-/**
- *  start local video preview, while not sending data to server
- *
- *  @return 0 when this method is called successfully, or negative value when this method failed.
+/** Starts the local video preview.
+
+ Before starting the preview, always call the setupLocalVideo:local: method to set up the preview window and configure its attributes and also call the enableVideo method to enable video. If before calling [joinChannelByToken](joinChannelByToken:channelId:info:uid:joinSuccess:) to join the channel, you have called startPreview to start the local video preview, then the local preview will be still in the started state after you called [leaveChannel]([AgoraLiveKit leaveChannel]) to exit the channel. You can call stopPreview to close the local preview.
+
+ @param mode Video display mode: AgoraVideoRenderMode
+ @return * 0: Success. * <0: Failure.
  */
 - (int)startPreview:(VIEW_CLASS *_Nonnull)view
          renderMode:(AgoraVideoRenderMode)mode;
 
-/**
- *  stop local video preview
- *
- *  @return 0 when this method is called successfully, or negative value when this method failed.
+/** Stops the local video preview.
+
+ @return * 0: Success. * <0: Failure.
  */
 - (int)stopPreview;
 
