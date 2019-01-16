8c7d06d7e0734d9241e94211eeb1937937547758
Hao Yang <tillohao@gmail.com>
2019-01-16 16:54:51 +0800

    update compile options to make everything works properly

--- js/Api/native_type.js
+++ js/Api/native_type.js
@@ -1,3 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmF0aXZlX3R5cGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi90cy9BcGkvbmF0aXZlX3R5cGUudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
