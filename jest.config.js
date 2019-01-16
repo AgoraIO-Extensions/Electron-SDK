7cca4b953279ecd139834e6555e493bfeb4f3f63
Hao Yang <tillohao@gmail.com>
2019-01-16 18:22:55 +0800

    update doc and test config
    Former-commit-id: 409d7ba62e4d0b5e12bfe7c81c7615e9e12041f4

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,9 @@
 module.exports = {
-	globals: {
-		'ts-jest': {
-			tsConfigFile: 'tsconfig.json'
-		}
-  },
   testPathIgnorePatterns: [
     "/__tests__/utils/"
   ],
   setupFiles: [
     "<rootDir>/__tests__/utils/mock.js"
   ],
-	moduleFileExtensions: [
-		'ts',
-		'js'
-	],
-	transform: {
-		'^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
-	},
   testEnvironment: 'jsdom'
 }
--- ts/Renderer/SoftwareRenderer/index.js
+++ ts/Renderer/SoftwareRenderer/index.js
@@ -162,4 +162,4 @@ class Renderer {
 
 }
 
-module.exports = Renderer;
+export default Renderer;
