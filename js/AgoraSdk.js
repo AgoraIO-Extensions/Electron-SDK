daf861d6a3552a523dd3415c877ce8e058327910
Hao Yang <tillohao@gmail.com>
2019-01-16 16:54:51 +0800

    update compile options to make everything works properly
    Former-commit-id: 760187fed7c0a50719064c2f497ac42293ecc947

--- js/AgoraSdk.js
+++ js/AgoraSdk.js
@@ -5,4 +5,3 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Api_1 = __importDefault(require("./Api"));
 exports.default = Api_1.default;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQWdvcmFTZGsuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9BZ29yYVNkay50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLGdEQUFtQztBQUNuQyxrQkFBZSxhQUFjLENBQUMifQ==
