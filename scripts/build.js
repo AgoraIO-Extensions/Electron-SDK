0048c654ae36204255f0ab758d8a3b3c73c55290
Hao Yang <tillohao@gmail.com>
2019-04-09 13:59:25 +0800

    <chore>   remove postbuild script to make ci work properly   fixed build script to generate xcode project file properly in debug env on macos

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,24 @@
-const fs = require('fs');
-const signale = require('signale');
-const shell = require('shelljs');
+const fs = require("fs");
+const signale = require("signale");
+const shell = require("shelljs");
 
 const {
   buildCommand,
   detectElectronVersion,
   detectOS,
-  detectOwnVersion,
-} = require('./utils');
+  detectOwnVersion
+} = require("./utils");
+const { Platform } = require("./constant");
 
 const detectEnv = () => {
   // get argv from command line
-  const {electron_version, runtime, debug, msvs_version, silent} = require('optimist').argv;
+  const {
+    electron_version,
+    runtime,
+    debug,
+    msvs_version,
+    silent
+  } = require("optimist").argv;
   // get os
   const platform = detectOS();
   // get pkg version
@@ -25,7 +32,7 @@ const detectEnv = () => {
     packageVersion: `${major}.${minor}.${patch}`,
     platform,
     dependentElectronVersion: dependentElectronVersion,
-    runtime: runtime || 'electron',
+    runtime: runtime || "electron",
     debug,
     msvs_version,
     silent
@@ -45,41 +52,64 @@ const main = () => {
 
   // build script
   const script = buildCommand({
-    platform, msvs_version, debug, runtime,
+    platform,
+    msvs_version,
+    debug,
+    runtime,
     electronVersion: dependentElectronVersion
   });
 
   console.log(script);
 
   // print build info
-  signale.info('Package Version =', packageVersion);
-  signale.info('Platform =', platform);
-  signale.info('Dependent Electron Version =', dependentElectronVersion);
-  signale.info('Build Runtime =', runtime, '\n');
+  signale.info("Package Version =", packageVersion);
+  signale.info("Platform =", platform);
+  signale.info("Dependent Electron Version =", dependentElectronVersion);
+  signale.info("Build Runtime =", runtime, "\n");
 
   // create two stream and start
-  signale.pending('Build C++ addon for Agora Electron SDK...\n');
-  const errLogWriteStream = fs.createWriteStream('error-log.txt', {
-    flags: 'a'
+  signale.pending("Build C++ addon for Agora Electron SDK...\n");
+  const errLogWriteStream = fs.createWriteStream("error-log.txt", {
+    flags: "a"
   });
-  const buildStream = shell.exec(script, {
-    silent: silent,
-  }, (code, stdout, stderr) => {
-    if (code !== 0) {
-      // failed
-      errLogWriteStream.write(stderr, 'utf8');
-      signale.fatal(
-        'Failed to build, check complete error log in',
-        shell.pwd() + '/error-log.txt\n'
-      );
-      process.exit(1);
-    } else {
-      signale.success('Build Complete');
-      process.exit(0);
+  const buildStream = shell.exec(
+    script,
+    {
+      silent: silent
+    },
+    (code, stdout, stderr) => {
+      if (code !== 0) {
+        // failed
+        errLogWriteStream.write(stderr, "utf8");
+        signale.fatal(
+          "Failed to build, check complete error log in",
+          shell.pwd() + "/error-log.txt\n"
+        );
+        process.exit(1);
+      } else {
+        signale.success("Build Complete");
+
+        // if macos in debug env
+        if (platform === Platform.MACOS && debug) {
+          // Generate xcode project file
+          shell.exec(
+            "node-gyp configure -- -f xcode",
+            { silent: silent },
+            (code, stdout, stderr) => {
+              if (code !== 0) {
+                signale.fatal("Failed to generate xcode project file");
+                process.exit(1);
+              } else {
+                signale.success("Succeed to generate xcode project file");
+                process.exit(0);
+              }
+            }
+          );
+        }
+        
+      }
     }
-  });
+  );
 };
 
 main();
-
-
