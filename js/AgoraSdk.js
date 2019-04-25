2bfe8d9ecfb52feae77dababdc2c5f70418bca40
Hao Yang <tillohao@gmail.com>
2019-04-25 14:38:36 +0800

    <chore> remove `/js /types /docs`, these files will be generated when neccessary (before test/publish/run example)

--- js/AgoraSdk.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Api_1 = __importDefault(require("./Api"));
-exports.default = Api_1.default;
