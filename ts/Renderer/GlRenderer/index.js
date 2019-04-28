d1392b69e93d48f436e09510dc56390479007c0b
rolfrando <rolfrando@gmail.com>
2019-04-27 21:33:23 -0700

    Support instantly refreshing view when view size is changed. (#44)
    * Support instantly refreshing view when view size is changed. Useful for low frame rates

--- ts/Renderer/GlRenderer/index.js
+++ ts/Renderer/GlRenderer/index.js
@@ -20,13 +20,16 @@ const AgoraRender = function() {
     initWidth: 0,
     initHeight: 0,
     initRotation: 0,
-    canvasUpdated: false,
+    // canvasUpdated: false,
     clientWidth: 0,
     clientHeight: 0,
     // 0 - cover, 1 - fit
     contentMode: 0,
     event: new EventEmitter(),
-    firstFrameRender: false
+    firstFrameRender: false,
+    lastImageWidth: 0,
+    lastImageHeight: 0,
+    lastImageRotation: 0
   };
 
   that.setContentMode = function(mode) {
@@ -65,7 +68,7 @@ const AgoraRender = function() {
     deleteBuffer(surfaceBuffer);
     texCoordBuffer = undefined;
     surfaceBuffer = undefined;
-    
+
     gl = undefined;
 
     try {
@@ -82,6 +85,12 @@ const AgoraRender = function() {
     that.view = undefined;
     that.mirrorView = false;
   };
+
+  that.refreshCanvas  = function() {
+    if (that.lastImageWidth) {
+      updateViewZoomLevel(that.lastImageRotation, that.lastImageWidth, that.lastImageHeight);
+    }
+  }
 
   that.renderImage = function(image) {
     // Rotation, width, height, left, top, right, bottom, yplane, uplane, vplane
@@ -152,11 +161,11 @@ const AgoraRender = function() {
       /**
    * draw image with params
    * @private
-   * @param {*} render 
-   * @param {*} header 
-   * @param {*} yplanedata 
-   * @param {*} uplanedata 
-   * @param {*} vplanedata 
+   * @param {*} render
+   * @param {*} header
+   * @param {*} yplanedata
+   * @param {*} uplanedata
+   * @param {*} vplanedata
    */
   that.drawFrame = function({header, yUint8Array, uUint8Array, vUint8Array}) {
     var headerLength = 20;
@@ -308,7 +317,7 @@ const AgoraRender = function() {
 
     that.view = view;
     that.mirrorView = mirror;
-    that.canvasUpdated = false;
+    // that.canvasUpdated = false;
 
     that.container = document.createElement('div');
     that.container.style.width = '100%';
@@ -409,10 +418,7 @@ const AgoraRender = function() {
     gl.uniform1i(v, 2); /* Bind Vtex to texture unit 2 */
   }
 
-  function updateCanvas(rotation, width, height) {
-    if (that.canvasUpdated) {
-      return;
-    }
+  function updateViewZoomLevel(rotation, width, height) {
     that.clientWidth = that.view.clientWidth;
     that.clientHeight = that.view.clientHeight;
 
@@ -451,9 +457,28 @@ const AgoraRender = function() {
       console.log(`updateCanvas 00001 gone ${that.canvas}`);
       console.log(that);
       console.error(e);
-      return;
+      return false;
     }
+    
+    return true
+  }
 
+  function updateCanvas(rotation, width, height) {
+    // if (that.canvasUpdated) {
+    //   return;
+    // }
+    if (width || height) {
+	    that.lastImageWidth = width;
+	    that.lastImageHeight = height;
+	    that.lastImageRotation = rotation;
+	  } else {
+	    width  = that.lastImageWidth;
+	    height = that.lastImageHeight;
+	    rotation = that.lastImageRotation;
+	  }
+    if (!updateViewZoomLevel(rotation, width, height)) {
+	    return;
+    }
     gl.bindBuffer(gl.ARRAY_BUFFER, surfaceBuffer);
     gl.enableVertexAttribArray(positionLocation);
     gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
@@ -517,7 +542,7 @@ const AgoraRender = function() {
 
     const resolutionLocation = gl.getUniformLocation(program, 'u_resolution');
     gl.uniform2f(resolutionLocation, width, height);
-    // That.canvasUpdated = true;
+    // that.canvasUpdated = true;
   }
 
   return that;
