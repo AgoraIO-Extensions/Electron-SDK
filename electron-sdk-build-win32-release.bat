9354d149065aac0b0caf14a36205e961c402a858
Qianze Zhang <qianze.zhang@hotmail.com>
2019-05-07 18:04:58 +0800

    Update electron-sdk-build-win32-release.bat

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -1,4 +1,4 @@
 cd %WORKSPACE%\Electron-SDK
 npm install --verbose
 node .\scripts\build.js --runtime=electron --electron_version=%ELECTRON_VERSION%
-7z a electron.zip build -r
+7z a electron.zip build\ -r
