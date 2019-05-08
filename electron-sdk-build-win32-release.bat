aa267db7e0a7647f155873379549e45276707be4
Qianze Zhang <zhangqianze@agora.io>
2019-05-08 18:32:05 +0800

    Update electron-sdk-build-win32-release.bat

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -3,4 +3,4 @@ call npm config set registry https://registry.npm.taobao.org/
 call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 call npm install --verbose
 call npm run build:electron%ELECTRON_VERSION%
-7z a electron.zip build\ -r
+7z a electron.zip build -r
