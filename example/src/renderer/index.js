d4acab93b42421e654a2b60c6cd43634730cc844
Qianze Zhang <zhangqianze@agora.io>
2019-05-16 10:18:51 +0800

    adapt example demo

--- example/src/renderer/index.js
+++ example/src/renderer/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
+import 'bulma-slider/dist/css/bulma-slider.min.css'
+import 'bulma-slider/dist/js/bulma-slider.min.js'
 import App from './App';
 import './index.css'
 
