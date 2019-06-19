b624456ac5bee25a497a04536dc5d54d56bbcef9
张乾泽 <qianze.zhang@hotmail.com>
2019-06-19 19:50:19 +0800

    fix release bug

--- wayang/webpack.renderer.additions.js
+++ wayang/webpack.renderer.additions.js
@@ -1,3 +1,9 @@
-module.exports = {
-    devtool: 'source-map'
+let configs = {
+    devtool: "source-map"
+};
+
+if(process.env.NODE_ENV !== 'development') {
+    configs.externals = "../../build/Release/agora_node_ext"
 }
+
+module.exports = configs
