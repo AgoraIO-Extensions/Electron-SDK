7bca8967ec813cca87a98f79259424795b22b109
Qianze Zhang <zhangqianze@agora.io>
2019-07-06 01:49:27 +0800

    fix
    Former-commit-id: 3d1851d168e998eefaf32af52186f86a544a980e

--- wayang/webpack.renderer.additions.js
+++
@@ -1,9 +0,0 @@
-let configs = {
-    devtool: "source-map"
-};
-
-if(process.env.NODE_ENV !== 'development') {
-    configs.externals = "../../build/Release/agora_node_ext"
-}
-
-module.exports = configs
