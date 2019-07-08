a35b5c55977f112db5038bd9c156d26c55596b72
张乾泽 <qianze.zhang@hotmail.com>
2019-07-08 10:53:23 +0800

    include js folder into build result
    Former-commit-id: 02616172dce393ab0f335799e3b7efbc3b7f01cd

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -4,4 +4,5 @@ npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 rm -rf node_modules
 npm install --verbose
 npm run build:electron -- --electron_version=${ELECTRON_VERSION} --msvs_version=2017
-zip -r electron.zip build
+npm run build:ts
+zip -r electron.zip build js
