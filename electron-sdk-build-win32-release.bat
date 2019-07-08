2d65c1ca8f3c5a2c38741782d441a6939d754025
张乾泽 <qianze.zhang@hotmail.com>
2019-07-08 10:53:23 +0800

    include js folder into build result

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -3,4 +3,5 @@ call npm config set registry https://registry.npm.taobao.org/
 call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 call npm install --verbose
 call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017
-7z a electron.zip build
+call npm run build:ts
+7z a electron.zip build js
