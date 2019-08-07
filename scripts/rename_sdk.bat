fc18dc59b8abaab733222f6b14648c5242c094f5
your name <your email>
2019-08-07 15:59:50 +0800

    fix win batch

--- scripts/rename_sdk.bat
+++ scripts/rename_sdk.bat
@@ -1,1 +1,3 @@
-for /f %a in ('dir /s /b /ad "Agora_Native_SDK_for_Windows*"') do move /y "%~a" "Agora_Native_SDK_for_Win_Full"
+cd %cd%
+dir /s /b /ad "%cd%\Agora_Native_SDK_for_Windows*"
+for /f %%a in ('dir /s /b /ad "%cd%\Agora_Native_SDK_for_Windows*"') do (move /y "%%~a" "Agora_Native_SDK_for_Win_Full")
