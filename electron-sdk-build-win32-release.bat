6d3c2e80a209860c9b9d5a4c69d603fe1b80a7ba
张乾泽 <qianze.zhang@hotmail.com>
2019-08-07 14:45:01 +0800

    adaopt win script

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -3,15 +3,15 @@ call curl %RTC_SDK_URL% -o NATIVE_SDK.zip
 
 
 call del /F /S /Q Agora_Native_SDK_for_Win_FULL
-call 7z e NATIVE_SDK.zip -oAgora_Native_SDK_for_Win_FULL
+call 7z x NATIVE_SDK.zip -oAgora_Native_SDK_for_Win_FULL
 
 call del /F /S /Q sdk/dll
 call del /F /S /Q sdk/include
 call del /F /S /Q sdk/lib/win/*
 
-call move /y ./Agora_Native_SDK_for_Win_FULL/sdk/dll ./sdk/.
-call move /y ./Agora_Native_SDK_for_Win_FULL/sdk/include ./sdk/.
-call move /y ./Agora_Native_SDK_for_Win_FULL/sdk/lib/* ./sdk/lib/win/.
+call move /y ./Agora_Native_SDK_for_Win_FULL/*/sdk/dll ./sdk/.
+call move /y ./Agora_Native_SDK_for_Win_FULL/*/sdk/include ./sdk/.
+call move /y ./Agora_Native_SDK_for_Win_FULL/*/sdk/lib/* ./sdk/lib/win/.
 
 call npm config set registry https://registry.npm.taobao.org/
 call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
