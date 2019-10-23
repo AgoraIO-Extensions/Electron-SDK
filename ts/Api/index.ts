3af616937253b786c9a942711baa7e3618afa92f
Qianze Zhang <qianze.zhang@hotmail.com>
2019-10-23 17:13:20 +0800

    Update index.ts

--- ts/Api/index.ts
+++ ts/Api/index.ts
@@ -504,7 +504,7 @@ class AgoraRtcEngine extends EventEmitter {
       fire('requestChannelKey');
     });
 
-    this.rtcEngine.onEvent('fristlocalaudioframe', function(elapsed: number) {
+    this.rtcEngine.onEvent('firstlocalaudioframe', function(elapsed: number) {
       fire('firstlocalaudioframe', elapsed);
       fire('firstLocalAudioFrame', elapsed);
     });
@@ -4967,7 +4967,7 @@ declare interface AgoraRtcEngine {
    * {@link joinChannel} until the
    * SDK triggers this callback.
    */
-  on(evt: 'fristLocalAudioFrame', cb: (elapsed: number) => void): this;
+  on(evt: 'firstLocalAudioFrame', cb: (elapsed: number) => void): this;
   /** Occurs when the engine receives the first audio frame from a specific 
    * remote user.
    * - uid: User ID of the remote user.
