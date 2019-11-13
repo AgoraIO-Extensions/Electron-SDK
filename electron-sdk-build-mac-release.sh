9c8b440b19ac0ca2023319d5d6b8592594884af4
张乾泽 <qianze.zhang@hotmail.com>
2019-11-13 18:05:44 +0800

    update package script

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -1,9 +1,4 @@
 cd ${WORKSPACE}/Electron-SDK
-curl ${RTC_SDK_URL} -o NATIVE_SDK.zip
-unzip NATIVE_SDK.zip
-
-rm -rf ./sdk/lib/mac/*
-mv ./Agora_Native_SDK_for_Mac_FULL/libs/AgoraRtcEngineKit.framework ./sdk/lib/mac/.
 
 npm config set registry https://registry.npm.taobao.org/
 npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
