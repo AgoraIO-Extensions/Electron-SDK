9d7657995316d2c08209d614676dca8d97796648
your name <your email>
2019-11-13 22:04:44 +0800

    fix win build error
    Former-commit-id: b4bea09f1e502fded3d2820ffb7b56e2d8ba4eee

--- agora_node_ext/agora_rtc_engine.cpp
+++ agora_node_ext/agora_rtc_engine.cpp
@@ -5584,3 +5584,37 @@ namespace agora {
         }
     }
 }
+
+#if defined(_WIN32)
+/*
+ * '_cups_strlcpy()' - Safely copy two strings.
+ */
+
+size_t                   /* O - Length of string */
+strlcpy(char *dst,       /* O - Destination string */
+        const char *src, /* I - Source string */
+        size_t size)     /* I - Size of destination string buffer */
+{
+    size_t srclen; /* Length of source string */
+
+    /*
+	* Figure out how much room is needed...
+	*/
+    size--;
+
+    srclen = strlen(src);
+
+    /*
+	* Copy the appropriate amount...
+	*/
+
+    if (srclen > size)
+        srclen = size;
+
+    memcpy(dst, src, srclen);
+    dst[srclen] = '\0';
+
+    return (srclen);
+}
+
+#endif
