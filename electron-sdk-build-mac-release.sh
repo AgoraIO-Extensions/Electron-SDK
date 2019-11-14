1f5185c7a324153d853cb97a9b0d28380c578fde
张乾泽 <qianze.zhang@hotmail.com>
2019-11-14 11:51:10 +0800

    remove sdk

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -3,6 +3,7 @@ npm config set registry https://registry.npm.taobao.org/
 npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 rm -rf node_modules
 npm install --verbose
+npm run sync:lib -- --liburl_mac=${RTC_SDK_URL}
 npm run build:electron -- --electron_version=${ELECTRON_VERSION} --msvs_version=2017
 npm run build:ts
 zip -r electron.zip build js
