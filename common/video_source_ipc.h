a7ecfe829af7c858581a065c9a73f92ab67e21df
QianzeZhang <zhangqianze@agora.io>
2019-12-04 13:47:42 +0800

    CSD-12892

--- common/video_source_ipc.h
+++ common/video_source_ipc.h
@@ -129,7 +129,7 @@ struct CaptureScreenByWinCmd
     agora::rtc::ScreenCaptureParameters captureParams;
 };
 
-#define MAX_TOKEN_LEN 128
+#define MAX_TOKEN_LEN 512
 #define MAX_CNAME_LEN 256
 #define MAX_CHAN_INFO 512
 #define MAX_PERMISSION_KEY 128
