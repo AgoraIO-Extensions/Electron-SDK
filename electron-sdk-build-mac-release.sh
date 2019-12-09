7db0aafb6f450c8f4766efbe552a98917ec4f344
QianzeZhang <zhangqianze@agora.io>
2019-12-10 01:36:12 +0800

    build script

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -2,5 +2,6 @@ cd ${WORKSPACE}/Electron-SDK
 npm config set registry https://registry.npm.taobao.org/
 npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 npm install --verbose
+npm run sync:lib -- --liburl_mac=${RTC_SDK_URL}
 npm run build:electron -- --electron_version=${ELECTRON_VERSION} --msvs_version=2017
 zip -r electron.zip build
