606a3e574fd3c226e9e9f895e90d12ef07320cc9
QianzeZhang <zhangqianze@agora.io>
2020-01-09 19:41:44 +0800

    use original dist url

--- electron-sdk-build-win64-release.bat
+++ electron-sdk-build-win64-release.bat
@@ -9,6 +9,6 @@ call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 call npm install --verbose
 call npm run switch:arch -- --arch=x64
 call npm run sync:lib -- --liburl_win=%RTC_SDK_URL% --arch=x64
-call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017 --arch=x64 --distUrl=https://npm.taobao.org/mirrors/atom-shell/
+call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017 --arch=x64
 call npm run build:ts
 7z a electron.zip build js
