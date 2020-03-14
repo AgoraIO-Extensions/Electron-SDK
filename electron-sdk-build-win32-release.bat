6422b1c6b8f749bcfb1005356649e4b708f3f8f8
QianzeZhang <zhangqianze@agora.io>
2020-03-15 01:16:40 +0800

    use mirror
    Former-commit-id: 1dfb7e6433caf798f67259727777dd158daa3fc5

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -9,6 +9,6 @@ call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 call npm install --verbose
 call npm run switch:arch
 call npm run sync:lib -- --liburl_win=%RTC_SDK_URL%
-call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017
+call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017 --distUrl=https://npm.taobao.org/mirrors/atom-shell/
 call npm run build:ts
 7z a electron.zip build js
