e54ab1060dc423f3f071c060bd1a96f703a83dc6
QianzeZhang <zhangqianze@agora.io>
2020-04-27 19:22:20 +0800

    support non-prebuilt

--- scripts/npm_argv.js
+++ scripts/npm_argv.js
@@ -8,7 +8,10 @@ module.exports.getArgvFromNpmEnv = () => {
     msvsVersion: process.env.npm_config_agora_electron_msvs_version,
     debug: process.env.npm_config_agora_electron_debug === 'true',
     silent: process.env.npm_config_agora_electron_silent === 'true',
-    arch: process.env.npm_config_agora_electron_arch
+    arch: process.env.npm_config_agora_electron_arch,
+    lib_sdk_win: process.env.npm_package_config_lib_sdk_win,
+    lib_sdk_win64: process.env.npm_package_config_lib_sdk_win64,
+    lib_sdk_mac: process.env.npm_package_config_lib_sdk_mac
   }
 }
 
@@ -16,7 +19,6 @@ module.exports.getArgvFromPkgJson = () => {
   const projectDir = path.join(process.env.INIT_CWD, 'package.json')
   const pkgMeta = require(projectDir);
   if (pkgMeta.agora_electron) {
-    const libUrl = pkgMeta.agora_electron.libUrl || {}
     return {
       electronVersion: pkgMeta.agora_electron.electron_version,
       prebuilt: pkgMeta.agora_electron.prebuilt === false ? false : true,
@@ -24,8 +26,7 @@ module.exports.getArgvFromPkgJson = () => {
       msvsVersion: pkgMeta.agora_electron.msvs_version,
       debug: pkgMeta.agora_electron.debug === true,
       silent: pkgMeta.agora_electron.silent === true,
-      arch: process.env.npm_config_agora_electron_arch,
-      libUrl
+      arch: process.env.npm_config_agora_electron_arch
     }
   } else {
     return {
