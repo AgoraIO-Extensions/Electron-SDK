f86abc2f38822bc2f7bf607ec0bb9f315a4d1669
QianzeZhang <zhangqianze@agora.io>
2020-04-27 16:46:00 +0800

    support non-prebuilt build

--- scripts/switch_arch.js
+++ scripts/switch_arch.js
@@ -5,11 +5,14 @@ const fs = require('fs-extra')
 module.exports = ({
   arch = 'ia32'
 }) => {
-    logger.info(`switching to ${arch}`)
-    if(arch === "ia32") {
-        // win32 config
-        fs.copyFileSync(path.join(__dirname, '../configs/binding32.gyp'), path.join(__dirname, '../binding.gyp'))
-    } else {
-        fs.copyFileSync(path.join(__dirname, '../configs/binding64.gyp'), path.join(__dirname, '../binding.gyp'))
-    }
+    return new Promise((resolve, reject) => {
+        logger.info(`switching to ${arch}`)
+        if(arch === "ia32") {
+            // win32 config
+            fs.copyFileSync(path.join(__dirname, '../configs/binding32.gyp'), path.join(__dirname, '../binding.gyp'))
+        } else {
+            fs.copyFileSync(path.join(__dirname, '../configs/binding64.gyp'), path.join(__dirname, '../binding.gyp'))
+        }
+        resolve()
+    })
 };
