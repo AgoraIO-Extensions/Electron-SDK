c77038e9430798d6446b167662d5c2c7282f6081
QianzeZhang <zhangqianze@agora.io>
2020-06-08 22:49:52 +0800

    fix npm script

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -3,9 +3,11 @@ curl ${RTC_SDK_URL} -o NATIVE_SDK.zip
 unzip NATIVE_SDK.zip
 
 rm -rf ./sdk/lib/mac/*
+
 mv ./Agora_Native_SDK_for_Mac_FULL/libs/AgoraRtcEngineKit.framework ./sdk/lib/mac/.
 
 rm -rf node_modules
+npm config delete registry
 npm install --verbose
 npm run sync:lib
 npm run build:electron -- --electron_version=${ELECTRON_VERSION} --msvs_version=2017
--- electron-sdk-build-win64-release.bat
+++ electron-sdk-build-win64-release.bat
@@ -4,7 +4,7 @@ call npm -v
 call node -v
 call npm config list
 del /f/q/s node_modules
-call npm cache clean --force
+call npm config delete registry
 call npm install --verbose
 call npm run switch:arch -- --arch=x64
 call npm run sync:lib -- --liburl_win=%RTC_SDK_URL% --arch=x64
