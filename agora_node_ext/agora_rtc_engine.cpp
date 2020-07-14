82cc54a08e9da5bcdd29d477045d97a3e4fb0b93
zhangtao1104 <1831369722@qq.com>
2020-07-14 14:27:58 +0800

    update code

--- agora_node_ext/agora_rtc_engine.cpp
+++ agora_node_ext/agora_rtc_engine.cpp
@@ -4704,7 +4704,7 @@ namespace agora {
                 unsigned int uid;
                 unsigned int size;
                 nodestring buffer;
-                long long timeStampMs;
+                double timeStampMs;
                 char *_buffer;
 
                 Local<Object> obj = args[0]->ToObject();
@@ -4715,7 +4715,7 @@ namespace agora {
                 status = napi_get_object_property_nodestring_(isolate, obj, "buffer", buffer);
                 _buffer = buffer;
                 CHECK_NAPI_STATUS(pEngine, status);
-                status = napi_get_object_property_int64_(isolate, obj, "timeStampMs", timeStampMs);
+                status = napi_get_object_property_double_(isolate, obj, "timeStampMs", timeStampMs);
                 CHECK_NAPI_STATUS(pEngine, status);
                 result = pEngine->metadataObserver.get()->sendMetadata(uid, size, reinterpret_cast<unsigned char *>(_buffer), timeStampMs);
             } while (false);
