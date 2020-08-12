5bf7ca1c2da702cc0eecfdee9034c79d2bf6447b
DongYifan <dongyifan@outlook.com>
2020-08-12 16:18:41 +0800

    Fix build error on Electron 9.0 (from 87de8d611c4665f352177d9dce9f93b3a76ac2a4)

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -7,6 +7,8 @@ mv ./Agora_Native_SDK_for_Mac_FULL/libs/AgoraRtcKit.framework ./sdk/lib/mac/.
 
 npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 rm -rf node_modules
+rm -rf sdk
+rm -rf tmp
 npm install --verbose
 npm run sync:lib
 npm run build:electron -- --electron_version=${ELECTRON_VERSION} --msvs_version=2017
