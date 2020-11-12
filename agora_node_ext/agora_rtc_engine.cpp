2baffb54321d5814bc86f26ded56359c55da4cd4
DongYifan <dongyifan@outlook.com>
2020-11-12 15:51:28 +0800

    Support sendCustomReportMessage

--- agora_node_ext/agora_rtc_engine.cpp
+++ agora_node_ext/agora_rtc_engine.cpp
@@ -292,6 +292,7 @@ namespace agora {
                 PROPERTY_METHOD_DEFINE(registerMediaMetadataObserver);
                 PROPERTY_METHOD_DEFINE(unRegisterMediaMetadataObserver);
 
+                PROPERTY_METHOD_DEFINE(sendCustomReportMessage);
             EN_PROPERTY_DEFINE()
             module->Set(context, Nan::New<v8::String>("NodeRtcEngine").ToLocalChecked(), tpl->GetFunction(context).ToLocalChecked());
         }
@@ -5502,6 +5503,37 @@ namespace agora {
                 status = napi_get_value_int32_(args[0], maxSize);
                 CHECK_NAPI_STATUS(pEngine, status);
                 result = pEngine->metadataObserver.get()->setMaxMetadataSize(maxSize);
+            } while (false);
+            napi_set_int_result(args, result);
+            LOG_LEAVE;
+        }
+
+        NAPI_API_DEFINE(NodeRtcEngine, sendCustomReportMessage)
+        {
+            LOG_ENTER;
+            int result = -1;
+            do {
+                NodeRtcEngine *pEngine = nullptr;
+                napi_status status = napi_ok;
+                napi_get_native_this(args, pEngine);
+                CHECK_NATIVE_THIS(pEngine);
+                nodestring id;
+                status = napi_get_value_nodestring_(args[0], id);
+                CHECK_NAPI_STATUS(pEngine, status);
+                nodestring category;
+                status = napi_get_value_nodestring_(args[1], category);
+                CHECK_NAPI_STATUS(pEngine, status);
+                nodestring event;
+                status = napi_get_value_nodestring_(args[2], event);
+                CHECK_NAPI_STATUS(pEngine, status);
+                nodestring label;
+                status = napi_get_value_nodestring_(args[3], label);
+                CHECK_NAPI_STATUS(pEngine, status);
+                int value;
+                status = napi_get_value_int32_(args[4], value);
+                CHECK_NAPI_STATUS(pEngine, status);
+
+                result = pEngine->m_engine->sendCustomReportMessage(id, category, event, label, value);
             } while (false);
             napi_set_int_result(args, result);
             LOG_LEAVE;
