8066c29b2ca105f586ee71d74ce0ad2c62486adb
zhangtao <zhangtao@agora.io>
2021-03-15 17:10:41 +0800

    modify version to dev/3.1.100.2

--- just-task.js
+++ just-task.js
@@ -73,7 +73,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '2.9.0-rc.107-build.1140'
+  const addonVersion = '3.1.100.2'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -89,7 +89,7 @@ task('download', () => {
 task('install', () => {
   const config = Object.assign({}, getArgvFromNpmEnv(), getArgvFromPkgJson())
   // work-around
-  const addonVersion = '2.9.0-rc.107-build.1140'
+  const addonVersion = '3.1.100.2'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
