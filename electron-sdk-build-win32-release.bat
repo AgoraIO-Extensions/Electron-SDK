6ad9bea6834327b5c477807db47a2ba70ff32bf2
zhangtao <zhangtao@agora.io>
2021-03-18 11:55:49 +0800

    modify msvs version to 2019

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -9,7 +9,7 @@ call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 call npm install --verbose
 call npm run switch:arch
 call npm run sync:lib -- --liburl_win=%RTC_SDK_URL%
-call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017
+call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2019
 call npm run build:ts
 D:\SignatureTools\SignatureTools\signtool.exe sign /f "D:\SignatureTools\SignatureTools\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build/Release/VideoSource.exe"
 D:\SignatureTools\SignatureTools\signtool.exe sign /f "D:\SignatureTools\SignatureTools\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build/Release/agora_node_ext.node"
