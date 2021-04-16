924e6597b2190691dea94596486f81240922fc11
Jerry.Luo <396912848@qq.com>
2021-04-16 17:15:00 +0800

    chore(hw version): 3.3.1.232

--- just-task.js
+++ just-task.js
@@ -73,7 +73,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.3.1'
+  const addonVersion = "3.3.1-rc.232";
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -89,7 +89,7 @@ task('download', () => {
 task('install', () => {
   const config = Object.assign({}, getArgvFromNpmEnv(), getArgvFromPkgJson())
   // work-around
-  const addonVersion = '3.3.1'
+  const addonVersion = "3.3.1-rc.232";
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
