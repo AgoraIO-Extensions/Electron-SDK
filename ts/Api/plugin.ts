f010aca6e0b363442be52f75cdc52029d67d91f0
zhangtao <zhangtao@agora.io>
2021-05-25 16:48:03 +0800

    update ts scripts

--- ts/Api/plugin.ts
+++ ts/Api/plugin.ts
@@ -2,7 +2,7 @@
  * @Author: zhangtao@agora.io
  * @Date: 2021-04-22 20:52:07
  * @Last Modified by: zhangtao@agora.io
- * @Last Modified time: 2021-05-10 20:26:45
+ * @Last Modified time: 2021-05-25 15:50:54
  */
 
 /**
@@ -18,7 +18,7 @@ export interface PluginInfo {
  * @ignore
  */
 export interface Plugin {
-  id: string;
+  pluginId: string;
   enable: () => number;
   disable: () => number;
   setParameter: (param: string) => number;
