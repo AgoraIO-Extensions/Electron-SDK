e3294cbdbfd2575fddf0a3220d4cbf63dcb49436
Jerry.Luo <396912848@qq.com>
2021-05-26 19:48:16 +0800

    electron version upgrade

--- example/src/main/index.js
+++ example/src/main/index.js
@@ -54,6 +54,7 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+app.allowRendererProcessReuse = false;
 
 app.on('activate', () => {
   // on macOS it is common to re-create a window even after all windows have been closed
