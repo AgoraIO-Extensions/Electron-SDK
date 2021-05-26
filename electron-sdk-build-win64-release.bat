daca9f46ff1fd356be7f3305bf032e1b43bd5354
Jerry.Luo <396912848@qq.com>
2021-05-26 13:18:56 +0800

    chore(msvs_version 2019): msvs_version

--- electron-sdk-build-win64-release.bat
+++ electron-sdk-build-win64-release.bat
@@ -9,7 +9,7 @@ call npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 call npm install --verbose
 call npm run switch:arch -- --arch=x64
 call npm run sync:lib -- --liburl_win=%RTC_SDK_URL% --arch=x64
-call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2017 --arch=x64
+call npm run build:electron -- --electron_version=%ELECTRON_VERSION% --msvs_version=2019 --arch=x64
 call npm run build:ts
 C:\SignatureTools\signtool.exe sign /f "C:\SignatureTools\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build/Release/VideoSource.exe"
 C:\SignatureTools\signtool.exe sign /f "C:\SignatureTools\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build/Release/agora_node_ext.node"
