4a5b5c3848d5c772deab01b3bb96e3084fd4c438
Jerry.Luo <396912848@qq.com>
2021-05-26 14:21:07 +0800

    ci(fix): fix

--- electron-sdk-build-mac-release.sh
+++ electron-sdk-build-mac-release.sh
@@ -9,6 +9,6 @@ npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 rm -rf node_modules
 npm install --verbose
 npm run sync:lib
-npm run build:electron -- --electron_version=${ELECTRON_VERSION} --msvs_version=2017
+npm run build:electron -- --electron_version=${ELECTRON_VERSION}
 npm run build:ts
 zip -r electron.zip build js
