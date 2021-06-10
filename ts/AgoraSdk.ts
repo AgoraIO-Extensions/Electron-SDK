2eaf5c76b97ee4a92e3ac5bac9932dd420f2ee5e
Jerry.Luo <396912848@qq.com>
2021-06-10 17:07:42 +0800

    replace engine event name

--- ts/AgoraSdk.ts
+++ ts/AgoraSdk.ts
@@ -4,10 +4,11 @@
  * @Last Modified by: zhangtao@agora.io
  * @Last Modified time: 2021-05-27 12:33:55
  */
-import AgoraRtcEngine from "./Api";
-
+import AgoraRtcEngine from "./Api/AgoraRtcEngine";
+export * from "./Api/AgoraRtcChannel";
 export * from "./Api/types";
 export * from "./Api/plugin";
 export * from "./Renderer/type";
+export * from "./Common/JSEvents";
 
 export default AgoraRtcEngine;
