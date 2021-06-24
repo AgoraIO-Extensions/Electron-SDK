dde1d719f4c163a9e19496f62ebb1bd319132305
Jerry.Luo <396912848@qq.com>
2021-06-24 21:43:55 +0800

    fix video source header

--- common/ipc_shm.h
+++ common/ipc_shm.h
@@ -807,8 +807,13 @@ template<uint32_t BLOCK_NUM, uint32_t BLOCK_SIZE>
 int32_t shm_ipc<BLOCK_NUM, BLOCK_SIZE>::read(int fd, void *buf, int32_t size)
 {
     shm_header<BLOCK_NUM, BLOCK_SIZE> *header = (shm_header<BLOCK_NUM, BLOCK_SIZE> *)shmOp->address();
-    if (fd < 0 || fd > header->channel_count)
+    if (header) {
+        if (fd < 0 || (header != NULL && fd > header->channel_count))
         return -1;
+    } else {
+        return -1;
+    }
+
 
     shm_channel<BLOCK_NUM, BLOCK_SIZE> *channel = &header->channels[fd];
     if (AtomicGet(&channel->used) != 1 ||
