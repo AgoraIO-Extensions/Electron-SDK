4aeb1c3c1d4817b7ee4974905e3f8bd0f1e19add
Jerry.Luo <396912848@qq.com>
2021-07-09 17:42:35 +0800

    3.4.2-iris.709-build.1

--- scripts/dowmloadPrebuild.js
+++ scripts/dowmloadPrebuild.js
@@ -11,7 +11,7 @@ const { electronVersion, platform, packageVersion, arch, no_symbol } =
 
 const workspaceDir = `${path.join(__dirname, "..")}`;
 
-const addonVersion = "3.4.2-iris.629-build.1";
+const addonVersion = "3.4.2-iris.701-build.1";
 
 const getDownloadURL = () => {
   let downloadUrl = `http://download.agora.io/sdk/release/Electron-${getOS()}-${addonVersion}-${electronVersion}.zip`;
