6af3b5cfa8d323900bdb20cd8a23f0f1df92a0c9
Jerry.Luo <396912848@qq.com>
2021-07-23 14:19:09 +0800

    update windows ci script

--- electron-sdk-build-win32-release.bat
+++ electron-sdk-build-win32-release.bat
@@ -4,10 +4,22 @@ call npm -v
 call node -v
 call npm config list
 call npm install --verbose --agora_electron_sdk_pre_built=false --agora_electron_version=%1 --agora_electron_sdk_arch=ia32
-C:\\SignatureTools\\signtool.exe sign /f "C:\\SignatureTools\\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build/Release/VideoSource.exe"
-C:\\SignatureTools\\signtool.exe sign /f "C:\\SignatureTools\\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build/Release/agora_node_ext.node"
-if %errorlevel% NEQ 0 (
-    echo "***** singtool Error *****"
+
+set count=0
+:loop
+set /a count=%count%+1
+if exist build\\Release\\VideoSource.exe (
+    C:\\SignatureTools\\signtool.exe sign /f "C:\\SignatureTools\\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build\\Release\\VideoSource.exe"
+)
+C:\\SignatureTools\\signtool.exe sign /f "C:\\SignatureTools\\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build\\Release\\agora_node_ext.node"
+
+if %errorlevel% equ 0 goto endloop
+if %count% neq 5 goto loop
+:endloop
+
+if %count% equ 5 (
+    echo "******sign error*******"
     exit 1
 )
+
 call npm run zipBuild
