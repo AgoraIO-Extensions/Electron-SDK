49ebeb0356208947364dfce6264194d469294127
Jerry.Luo <396912848@qq.com>
2021-08-13 12:13:30 +0800

    3.4.7-rc.1-build.813

--- just-task.js
+++ just-task.js
@@ -116,7 +116,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.4.6-build.730'
+  const addonVersion = '3.4.7.rc-1-build.812'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -137,7 +137,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.4.6-build.730'
+  const addonVersion = '3.4.7.rc-1-build.812'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
