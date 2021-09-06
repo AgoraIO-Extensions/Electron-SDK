67243080e4ae7f1cd3b08850ce93e55f5915cc3f
Jerry.Luo <396912848@qq.com>
2021-09-06 11:04:21 +0800

    3.4.7-rc.122-build.906

--- just-task.js
+++ just-task.js
@@ -113,7 +113,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.4.7-rc.1-build.817'
+  const addonVersion = '3.4.7-rc.122-build.906'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -134,7 +134,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.4.7-rc.1-build.817'
+  const addonVersion = '3.4.7-rc.122-build.906'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
