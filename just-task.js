f6c6fa95b3cbd2b5a9560ba20bc9f16b201f4076
Jerry.Luo <396912848@qq.com>
2021-11-23 14:15:30 +0800

    3.5.1-build.1123

--- just-task.js
+++ just-task.js
@@ -116,7 +116,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.5.1-build.1116'
+  const addonVersion = '3.5.1-build.1123'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -137,7 +137,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.5.1-build.1116'
+  const addonVersion = '3.5.1-build.1123'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
