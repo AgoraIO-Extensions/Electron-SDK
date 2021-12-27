216995ef92957888defcfbe10fb715df3d53912b
Jerry.Luo <396912848@qq.com>
2021-12-27 23:56:58 +0800

    Csd/37623/dev/3.6.0 (#618)
    * fix ConvertRGBToBMP
    
    * 3.6.0-build.1228

--- just-task.js
+++ just-task.js
@@ -116,7 +116,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.6.0-build.1216'
+  const addonVersion = '3.6.0-build.1230'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -137,7 +137,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.6.0-build.1216'
+  const addonVersion = '3.6.0-build.1230'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
