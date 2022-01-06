28709494efa107b37a00a64c0abaf2af584346ae
Jerry.Luo <396912848@qq.com>
2022-01-06 04:13:43 -0800

    Dev/lh/dev/3.6.0 (#631)
    * fix windows max path
    
    * 3.6.0-build.106

--- just-task.js
+++ just-task.js
@@ -116,7 +116,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.6.0-build.1231'
+  const addonVersion = '3.6.0-build.106'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -137,7 +137,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.6.0-build.1231'
+  const addonVersion = '3.6.0-build.106'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
