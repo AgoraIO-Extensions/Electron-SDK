e7b92d2fce06b1eff900a511c2e5804e7bc0f9e2
Jerry.Luo <396912848@qq.com>
2022-03-24 11:09:06 +0800

    3.6.1-rc.4-build.324

--- just-task.js
+++ just-task.js
@@ -116,7 +116,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.6.1-rc.4-build.310';
+  const addonVersion = '3.6.1-rc.4-build.324';
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -137,7 +137,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.6.1-rc.4-build.310';
+  const addonVersion = '3.6.1-rc.4-build.324';
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
