9c789787bdc69c7ab7b474f0a78050c8b4f32b9f
Luohao <luohao@agora.io>
2022-04-13 20:26:53 +0800

    3.7.204-build.413

--- just-task.js
+++ just-task.js
@@ -74,7 +74,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.7.204-build.412'
+  const addonVersion = '3.7.204-build.413'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -90,7 +90,7 @@ task('download', () => {
 task('install', () => {
   const config = Object.assign({}, getArgvFromNpmEnv(), getArgvFromPkgJson())
   // work-around
-  const addonVersion = '3.7.204-build.412'
+  const addonVersion = '3.7.204-build.413'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
