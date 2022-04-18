e66b28dc49b41b7edc0391be8d3ee2494113f5bc
zhangtao <1831369722@qq.com>
2022-04-18 18:30:00 +0800

    Revert "update startTertiaryCameraCapture startQuaternaryCameraCapture stopTertiaryCameraCapture stopQuaternaryCameraCapture (#703)"
    This reverts commit b13ccc9d24611a100afe084b5231f52b6aab2338.

--- agora_node_ext/agora_rtc_engine.h
+++ agora_node_ext/agora_rtc_engine.h
@@ -333,12 +333,6 @@ public:
   NAPI_API(startSecondaryScreenCapture);
   NAPI_API(stopPrimaryScreenCapture);
   NAPI_API(stopSecondaryScreenCapture);
-
-  NAPI_API(startTertiaryCameraCapture);
-  NAPI_API(startQuaternaryCameraCapture);
-  NAPI_API(stopTertiaryCameraCapture);
-  NAPI_API(stopQuaternaryCameraCapture);
-
   NAPI_API(adjustLoopbackRecordingVolume);
   NAPI_API(setCameraDeviceOrientation);
 
