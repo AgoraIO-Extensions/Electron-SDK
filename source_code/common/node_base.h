e02109b1e8958bfa2e949c608b9abd572356265c
zhangtao <zhangtao@agora.io>
2022-05-28 21:02:05 +0800

    update source code

--- source_code/common/node_base.h
+++ source_code/common/node_base.h
@@ -1,6 +1,37 @@
 #pragma once
 #include <node_api.h>
 #include "logger.h"
+#if defined(__GUNC__)
+#define COMPILER_IS_GCC
+#if defined(__MINGW32__) || defined(__MINGW64__)
+#define COMPILER_IS_MINGW
+#endif
+#if defined(__MSYS__)
+#define COMPILER_ON_MSYS
+#endif
+#if defined(__CYGWIN__) || defined(__CYGWIN32__)
+#define COMPILER_ON_CYGWIN
+#endif
+#if defined(__clang__)
+#define COMPILER_IS_CLANG
+#endif
+#elif defined(_MSC_VER)
+#define COMPILER_IS_MSVC
+#else
+#define COMPILER_IS_UNKNOWN
+#endif
+
+#if defined(COMPILER_IS_MSVC)
+typedef __int64 int64;
+typedef unsigned __int64 uint64;
+#elif (defined(__LONG_WIDTH__) && __LONG_WIDTH__ == 8) ||                      \
+    (defined(__SIZEOF_LONG__) && __SIZEOF_LONG__ == 8)
+typedef signed long int64;
+typedef unsigned long uint64;
+#else
+typedef signed long long int64;
+typedef unsigned long long uint64;
+#endif
 
 namespace agora {
 namespace rtc {
