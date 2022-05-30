b84b360791bfcfa9c9134554759761357e99ca9f
Jerry.Luo <396912848@qq.com>
2022-05-30 20:40:29 +0800

    format export

--- ts/AgoraSdk.ts
+++ ts/AgoraSdk.ts
@@ -1,12 +1,12 @@
 import { AgoraRtcEngine } from "./AgoraRtcEngine";
 import "./Renderer/AgoraView";
-
-export * from "./Types";
-export * from "./Renderer/RendererManager";
-export { AgoraEnv } from "./Utils";
-
 export * from "./Private/AgoraBase";
 export * from "./Private/AgoraMediaBase";
 export * from "./Private/IAgoraRtcEngineEx";
+export * from "./Private/impl/IAgoraRtcEngineImpl";
+export * from "./Private/impl/IAudioDeviceManagerImpl";
+export * from "./Renderer/RendererManager";
+export * from "./Types";
+export { AgoraEnv } from "./Utils";
 
 export default AgoraRtcEngine;
