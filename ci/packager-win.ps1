da83fd95fc0531b6fcdd743eec44fd3763543774
Jerry.Luo <396912848@qq.com>
2022-06-20 17:50:36 +0800

    update ci

--- ci/packager-win.ps1
+++ ci/packager-win.ps1
@@ -1,4 +1,5 @@
 $chooseExampleType=$args[0]
+$electronVersion=$args[1]
 $outterZipName="electronDemo.zip"
 
 
@@ -6,10 +7,10 @@ function ChooseArch($type)
 {
   if($type -eq 1){
     write-host("ChooseArch x32")
-    Copy-Item -Path ../.npmrc_x32 -Destination ./.npmrc -Force
+    Copy-Item -Path ../ci/.npmrc_x32 -Destination ./.npmrc -Force
   } elseif($type -eq 2){
     write-host("ChooseArch x64")
-    Copy-Item -Path ../.npmrc_x64 -Destination ./.npmrc -Force
+    Copy-Item -Path ../ci/.npmrc_x64 -Destination ./.npmrc -Force
   }else {
     write-host("not set arch type")
   }
@@ -28,10 +29,11 @@ function DistByArch($type)
     write-host("not set arch type")
   }
 }
-function Package($archNum){
+
+function Package($archNum,$electronVersion){
   # remove zip
   Remove-Item -Path $outterZipName -Recurse -Force -ErrorAction Ignore;
-  pushd Agora-Electron-API-Example
+  pushd example
   # choose arch
   ChooseArch -type $archNum
   # remove node_modules
@@ -39,7 +41,17 @@ function Package($archNum){
   
   # remove dist
   Remove-Item -Path dist -Recurse -Force -ErrorAction Ignore;
-  yarn
+  If([String]::IsNullOrEmpty($electronVersion))
+  {
+    Write-Host "选择了 electron_version:$electronVersion"
+    yarn add electron@$electronVersion --verbose
+  }
+  Else
+  {
+    Write-Host "安装example 依赖"
+    yarn --verbose
+  }
+
   # copy native sdk
   Copy-Item -Path ../Electron-*/* -Destination src/node_modules/electron-agora-rtc-ng/ -Recurse -Force
   # dist start
@@ -49,50 +61,29 @@ function Package($archNum){
   popd;
 }
 
-switch -Regex ($chooseExampleType)
-{
-    1 {
-      write-host("Package win:1")
-      Package -archNum $args[1]
-      Break
-    }
-    2 {
-      write-host("Package win:2")
-      Package -archNum $args[1]
-      Break
-    }
-    3 {
-      write-host("Package win:3")
-      Package -archNum $args[1]
-      Break
-    }
-    4 {"It is four."; Break}
-}
+write-host("Package win:1")
+Package -archNum $chooseExampleType -electronVersion $electronVersion
+
+# switch -Regex ($chooseExampleType)
+# {
+#     1 {
+#       write-host("Package win:1")
+#       Package -archNum $args[1]
+#       Break
+#     }
+#     2 {
+#       write-host("Package win:2")
+#       Package -archNum $args[1]
+#       Break
+#     }
+#     3 {
+#       write-host("Package win:3")
+#       Package -archNum $args[1]
+#       Break
+#     }
+#     4 {"It is four."; Break}
+# }
 
 
 echo "结束"
-
-# IF "%chooseExampleType%"=="0" (
-#   cd Agora-Electron-Premium
-#   echo 当前工作路径: %cd%
-
-#   @REM del /f/q/s node_modules
-#   @REM call yarn --verbose
-  
-#   xcopy %cd%/Electron-*  node_modules/electron-agora-rtc-ng/ /s /e /y
-#   @REM call yarn dist:zip
-
-#   @REM pushd dist
-#   @REM 7z a %cd%/../../../%outterZipName% win-unpacked
-#   @REM popd
-#   @REM popd
-# ) ELSE IF "%chooseExampleType%"=="1" (
-#   echo 1
-# ) ELSE IF "%chooseExampleType%"=="2" (
-  
-#   echo 2
-# ) ELSE (
-  
-#   echo default
-# )
 
