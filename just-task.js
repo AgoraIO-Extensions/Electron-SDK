6b45068db1827e09d1f3acc39396aa5840e8cfd1
zhangtao <zhangtao@agora.io>
2022-06-20 11:21:14 +0800

    Dev/zhangtao (#773)
    * adapte 3.8.202
    
    * 3.8.202-build.619

--- just-task.js
+++ just-task.js
@@ -74,7 +74,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.8.200-build.614'
+  const addonVersion = '3.8.202-build.619'
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -90,7 +90,7 @@ task('download', () => {
 task('install', () => {
   const config = Object.assign({}, getArgvFromNpmEnv(), getArgvFromPkgJson())
   // work-around
-  const addonVersion = '3.8.200-build.614'
+  const addonVersion = '3.8.202-build.619'
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
