0c737158fbecca3980c7e90dea24d3cd1a067b4b
Jerry.Luo <396912848@qq.com>
2022-06-20 17:50:36 +0800

    update ci

--- ci/electron-sdk-build-mac-release.sh
+++ ci/electron-sdk-build-mac-release.sh
@@ -1,6 +1,6 @@
 npm config set ELECTRON_MIRROR http://npm.taobao.org/mirrors/electron/
 # Example 1
-npm config set AGORA_ELECTRON_SDK_PRE_BUILT false
+# npm config set AGORA_ELECTRON_SDK_PRE_BUILT false
 
 rm -rf node_modules
 rm -rf sdk
@@ -8,8 +8,8 @@ rm -rf tmp
 # npm install --verbose
 #
 # Example 2
-npm install --verbose --agora_electron_sdk_pre_built=false
-npm run zipBuild
+yarn --verbose --agora_electron_sdk_pre_built=false
+yarn zipBuild
 #
 # Example 3
 # npm run build  -- --prebuilt=false --electronVersion=5.0.8 --arch=x64
