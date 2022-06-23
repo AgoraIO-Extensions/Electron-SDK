47c96a2fcc3dec7756638de1dbca6bc2cc206d1a
Jerry.Luo <396912848@qq.com>
2022-06-23 19:57:54 +0800

    update ci

--- example/src/renderer/examples/advanced/SendMetaData/SendMetaData.scss.d.ts
+++ example/src/renderer/examples/advanced/SendMetaData/SendMetaData.scss.d.ts
@@ -1,14 +1,14 @@
 declare namespace SendMetaDataScssNamespace {
   export interface ISendMetaDataScss {
-    msg: string;
-    msgList: string;
-    toolBarContent: string;
+    msg: string
+    msgList: string
+    toolBarContent: string
   }
 }
 
 declare const SendMetaDataScssModule: SendMetaDataScssNamespace.ISendMetaDataScss & {
   /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
-  locals: SendMetaDataScssNamespace.ISendMetaDataScss;
-};
+  locals: SendMetaDataScssNamespace.ISendMetaDataScss
+}
 
-export = SendMetaDataScssModule;
+export = SendMetaDataScssModule
