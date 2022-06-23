dfa29d784e315499d1cff3a413a149138b4bb81f
Jerry.Luo <396912848@qq.com>
2022-06-23 19:57:54 +0800

    update ci

--- example/src/renderer/examples/advanced/CreateDataStream/CreateDataStream.scss.d.ts
+++ example/src/renderer/examples/advanced/CreateDataStream/CreateDataStream.scss.d.ts
@@ -1,14 +1,14 @@
 declare namespace CreateDataStreamScssNamespace {
   export interface ICreateDataStreamScss {
-    msg: string;
-    msgList: string;
-    toolBarContent: string;
+    msg: string
+    msgList: string
+    toolBarContent: string
   }
 }
 
 declare const CreateDataStreamScssModule: CreateDataStreamScssNamespace.ICreateDataStreamScss & {
   /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
-  locals: CreateDataStreamScssNamespace.ICreateDataStreamScss;
-};
+  locals: CreateDataStreamScssNamespace.ICreateDataStreamScss
+}
 
-export = CreateDataStreamScssModule;
+export = CreateDataStreamScssModule
