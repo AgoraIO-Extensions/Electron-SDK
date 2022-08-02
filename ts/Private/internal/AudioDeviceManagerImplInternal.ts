1f6aca9b9317d644c9b06ac10b372a1c1fcffc5c
HUI <luxuhui@agora.io>
2022-08-02 13:11:12 +0800

    fix: setPlayerOptionInInt setPlayerOptionInString this issue

--- ts/Private/internal/AudioDeviceManagerImplInternal.ts
+++ ts/Private/internal/AudioDeviceManagerImplInternal.ts
@@ -6,7 +6,7 @@ export class AudioDeviceManagerImplInternal extends IAudioDeviceManagerImpl {
   override getPlaybackDeviceInfo(): AudioDeviceInfo {
     const apiType = "AudioDeviceManager_getPlaybackDeviceInfo";
     const jsonParams = {};
-    const jsonResults = callIrisApi(apiType, jsonParams);
+    const jsonResults = callIrisApi.call(this, apiType, jsonParams);
     if (jsonResults.result === 0) {
       delete jsonResults.result;
     }
@@ -16,7 +16,7 @@ export class AudioDeviceManagerImplInternal extends IAudioDeviceManagerImpl {
   override getRecordingDeviceInfo(): AudioDeviceInfo {
     const apiType = "AudioDeviceManager_getRecordingDeviceInfo";
     const jsonParams = {};
-    const jsonResults = callIrisApi(apiType, jsonParams);
+    const jsonResults = callIrisApi.call(this, apiType, jsonParams);
     if (jsonResults.result === 0) {
       delete jsonResults.result;
     }
