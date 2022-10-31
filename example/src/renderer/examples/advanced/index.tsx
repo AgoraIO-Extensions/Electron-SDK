c2d8de65ca37ad3b2e02b56461ddb3b3bc3c5122
HUI <luxuhui@agora.io>
2022-10-31 19:40:56 +0800

    feat: add ProcessVideoRawData.tsx

--- example/src/renderer/examples/advanced/index.tsx
+++ example/src/renderer/examples/advanced/index.tsx
@@ -27,6 +27,7 @@ import Extension from './Extension/Extension';
 import LocalSpatialAudioEngine from './LocalSpatialAudioEngine/LocalSpatialAudioEngine';
 import DeviceManager from './DeviceManager/DeviceManager';
 import MusicContentCenter from './MusicContentCenter/MusicContentCenter';
+import ProcessVideoRawData from './ProcessVideoRawData/ProcessVideoRawData';
 
 const advanceRoute = [
   { path: '/AudioMixing', component: AudioMixing, title: 'AudioMixing' },
@@ -82,6 +83,11 @@ const advanceRoute = [
     title: 'MusicContentCenter',
   },
   { path: '/PlayEffect', component: PlayEffect, title: 'PlayEffect' },
+  {
+    path: '/ProcessVideoRawData',
+    component: ProcessVideoRawData,
+    title: 'ProcessVideoRawData',
+  },
   {
     path: '/PushVideoFrame',
     component: PushVideoFrame,
