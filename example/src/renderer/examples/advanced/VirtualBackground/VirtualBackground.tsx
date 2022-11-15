196de037e425a6b8ec3d1c1e7720f9392f7b3a72
HUI <luxuhui@agora.io>
2022-11-15 15:39:16 +0800

    chore: optimize

--- example/src/renderer/examples/advanced/VirtualBackground/VirtualBackground.tsx
+++ example/src/renderer/examples/advanced/VirtualBackground/VirtualBackground.tsx
@@ -73,7 +73,6 @@ export default class VirtualBackground
     this.engine?.enableExtension(
       'agora_video_filters_segmentation',
       'portrait_segmentation',
-      {},
       true
     );
 
