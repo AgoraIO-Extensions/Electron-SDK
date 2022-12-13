4f5e633d724347b695f6bb6c3a4deb0b29d7d195
HUI <luxuhui@agora.io>
2022-12-13 18:25:37 +0800

    ci: optimize

--- ci/build/build_mac.groovy
+++ ci/build/build_mac.groovy
@@ -40,20 +40,47 @@ def doPublish(buildVariables) {
     (shortVersion, releaseVersion) = buildUtils.getBranchVersion()
     def archiveInfos = [
         [
-          "type": "ARTIFACTORY",
-          "archivePattern": "*.zip",
-          "serverPath": "ELECTRON/${shortVersion}/${buildVariables.buildDate}/${env.platform}",
-          "serverRepo": "AD_repo" // ATTENTIONS: Update the artifactoryRepo if needed.
+          'type': 'ARTIFACTORY',
+          'archivePattern': '*.zip',
+          'serverPath': "ELECTRON/${params.network_path}/${env.platform}",
+          'serverRepo': 'CSDC_repo' // ATTENTIONS: Update the artifactoryRepo if needed.
         ]
     ]
-    archiveUrls = archive.archiveFiles(archiveInfos) ?: []
-    archiveUrls = archiveUrls as Set
-    if (archiveUrls) {
-        def content = archiveUrls.join("\n")
-        writeFile(file: 'package_urls', text: content, encoding: "utf-8")
+    def artifactoryUrls = archive.archiveFiles(archiveInfos)
+    if (params.Upload_CDN) {
+        doUploadCDN(artifactoryUrls)
+    }
+    sh 'rm -rf *.zip || true'
+    if (params.isBuildDemo) {
+        def payload1 = """
+                    {
+                        "msgtype": "text",
+                        "text": {
+                            "content": \"${env.NOTIFICATION_CONTENT}\n${artifactoryUrls.find { it.contains('demo') }}\"
+                        }
+                    }
+                    """
+        httpRequest httpMode: 'POST',
+                    acceptType: 'APPLICATION_JSON_UTF8',
+                    contentType: 'APPLICATION_JSON_UTF8',
+                    ignoreSslErrors: true, responseHandle: 'STRING',
+                    requestBody: payload1,
+                    url: 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${env.NOTIFICATION_KEY}'
     }
     archiveArtifacts(artifacts: "package_urls", allowEmptyArchive:true)
     sh "rm -rf *.zip || true"
 }
 
-pipelineLoad(this, "ELECTRON", "build", "mac", "electron_mac")
+def doUploadCDN(artifactoryUrls) {
+    if (!artifactoryUrls) {
+        return
+    }
+    def cdnUrl = artifactoryUrls.find { !it.contains('demo') }.replace('artifactory-api.bj2.agoralab.co', 'artifactory.agoralab.co')
+    build job: 'AD/Agora-Electron-Upload-CDN', propagate: false, parameters: [
+        string(name: 'electron_sdk_url', value: cdnUrl),
+        string(name: 'npmv', value: params.package_version),
+        string(name: 'electron_version', value: 'napi')
+    ], wait: true
+}
+
+pipelineLoad(this, 'ELECTRON', 'build', 'mac', 'electron_mac')
