81b989e920997b0fbd34403241a2de040253c981
HUI <luxuhui@agora.io>
2022-12-20 20:27:31 +0800

    build: fix ci build failed

--- ci/electron-sdk-build-mac-release.sh
+++ ci/electron-sdk-build-mac-release.sh
@@ -8,7 +8,8 @@ rm -rf tmp
 # npm install --verbose
 #
 # Example 2
-npm install --agora_electron_sdk_pre_built=false
+npm install --verbose --agora_electron_sdk_pre_built=false
+npm run totalBuild --verbose
 npm run zipBuild
 #
 # Example 3
