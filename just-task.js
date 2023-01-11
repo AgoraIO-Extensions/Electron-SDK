5191ce7af5cf5ce578017a2aebdaf219a529533c
Arthas <15215604969@163.com>
2023-01-11 10:48:07 +0800

    chore: upgrade native SDK to 3.4.11 (#938)
    Co-authored-by: 刘墉 <liuyong@ADMINdeMacBook-Pro-5.local>

--- just-task.js
+++ just-task.js
@@ -116,7 +116,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '3.4.11-build.1228'
+  const addonVersion = packageVersion
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -137,7 +137,7 @@ task('install', () => {
   }
   
   // work-around
-  const addonVersion = '3.4.11-build.1228'
+  const addonVersion = packageVersion
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
