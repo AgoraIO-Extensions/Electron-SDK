c0dc4a43964697306a0d46c714d978171053da62
Arthas <15215604969@163.com>
2023-01-11 19:42:49 +0800

    chore: upgrade native SDK to 2.9.106.243 (#939)
    * chore: upgrade native SDK to 2.9.106.243
    
    * Update package.json
    
    Co-authored-by: 刘墉 <liuyong@ADMINdeMacBook-Pro-5.local>

--- just-task.js
+++ just-task.js
@@ -72,7 +72,7 @@ task('build:node', () => {
 // npm run download --
 task('download', () => {
   // work-around
-  const addonVersion = '2.9.106-rc.243-build.419'
+  const addonVersion = packageVersion
   cleanup(path.join(__dirname, "./build")).then(_ => {
     cleanup(path.join(__dirname, './js')).then(_ => {
       download({
@@ -88,7 +88,7 @@ task('download', () => {
 task('install', () => {
   const config = Object.assign({}, getArgvFromNpmEnv(), getArgvFromPkgJson())
   // work-around
-  const addonVersion = '2.9.106-rc.243-build.419'
+  const addonVersion = packageVersion
   if (config.prebuilt) {
     download({
       electronVersion: config.electronVersion, 
