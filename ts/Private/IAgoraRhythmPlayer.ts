c78d4a6c96b4204d2d16653d1024d012f483d7e3
LichKing-2234 <LichKing-2234@users.noreply.github.com>
2023-03-02 09:00:13 +0000

    [AUTO] Update doc

--- ts/Private/IAgoraRhythmPlayer.ts
+++ ts/Private/IAgoraRhythmPlayer.ts
@@ -1,36 +1,60 @@
 import './extension/IAgoraRhythmPlayerExtension';
-/* enum_rhythmplayerstatetype */
+/**
+ * @ignore
+ */
 export enum RhythmPlayerStateType {
-/* enum_rhythmplayerstatetype_RhythmPlayerStateIdle */
-RhythmPlayerStateIdle = 810,
-/* enum_rhythmplayerstatetype_RhythmPlayerStateOpening */
-RhythmPlayerStateOpening = 811,
-/* enum_rhythmplayerstatetype_RhythmPlayerStateDecoding */
-RhythmPlayerStateDecoding = 812,
-/* enum_rhythmplayerstatetype_RhythmPlayerStatePlaying */
-RhythmPlayerStatePlaying = 813,
-/* enum_rhythmplayerstatetype_RhythmPlayerStateFailed */
-RhythmPlayerStateFailed = 814,
+  /**
+   * @ignore
+   */
+  RhythmPlayerStateIdle = 810,
+  /**
+   * @ignore
+   */
+  RhythmPlayerStateOpening = 811,
+  /**
+   * @ignore
+   */
+  RhythmPlayerStateDecoding = 812,
+  /**
+   * @ignore
+   */
+  RhythmPlayerStatePlaying = 813,
+  /**
+   * @ignore
+   */
+  RhythmPlayerStateFailed = 814,
 }
 
-/* enum_rhythmplayererrortype */
+/**
+ * @ignore
+ */
 export enum RhythmPlayerErrorType {
-/* enum_rhythmplayererrortype_RhythmPlayerErrorOk */
-RhythmPlayerErrorOk = 0,
-/* enum_rhythmplayererrortype_RhythmPlayerErrorFailed */
-RhythmPlayerErrorFailed = 1,
-/* enum_rhythmplayererrortype_RhythmPlayerErrorCanNotOpen */
-RhythmPlayerErrorCanNotOpen = 801,
-/* enum_rhythmplayererrortype_RhythmPlayerErrorCanNotPlay */
-RhythmPlayerErrorCanNotPlay = 802,
-/* enum_rhythmplayererrortype_RhythmPlayerErrorFileOverDurationLimit */
-RhythmPlayerErrorFileOverDurationLimit = 803,
+  /**
+   * @ignore
+   */
+  RhythmPlayerErrorOk = 0,
+  /**
+   * @ignore
+   */
+  RhythmPlayerErrorFailed = 1,
+  /**
+   * @ignore
+   */
+  RhythmPlayerErrorCanNotOpen = 801,
+  /**
+   * @ignore
+   */
+  RhythmPlayerErrorCanNotPlay = 802,
+  /**
+   * @ignore
+   */
+  RhythmPlayerErrorFileOverDurationLimit = 803,
 }
 
-/* class_agorarhythmplayerconfig */
+/**
+ * The metronome configuration.
+ */
 export class AgoraRhythmPlayerConfig {
-  /* class_agorarhythmplayerconfig_beatsPerMeasure */
-  beatsPerMeasure?: number
-  /* class_agorarhythmplayerconfig_beatsPerMinute */
-  beatsPerMinute?: number
+  beatsPerMeasure?: number;
+  beatsPerMinute?: number;
 }
--- ts/Private/IAudioDeviceManager.ts
+++ ts/Private/IAudioDeviceManager.ts
@@ -1,99 +1,281 @@
 import './extension/IAudioDeviceManagerExtension';
-/* enum_maxdeviceidlengthtype */
+/**
+ * The maximum length of the device ID.
+ */
 export enum MaxDeviceIdLengthType {
-/* enum_maxdeviceidlengthtype_MaxDeviceIdLength */
-MaxDeviceIdLength = 512,
+  /**
+   * The maximum length of the device ID is 512 bytes.
+   */
+  MaxDeviceIdLength = 512,
 }
 
-/* class_iaudiodevicemanager */
+/**
+ * Audio device management methods.
+ */
 export abstract class IAudioDeviceManager {
-/* api_iaudiodevicemanager_enumerateplaybackdevices */
-abstract enumeratePlaybackDevices(): AudioDeviceInfo[];
+  /**
+   * Enumerates the audio playback devices.
+   *
+   * @returns
+   * Success: Returns an AudioDeviceInfo array, which includes all the audio playback devices.Failure: An empty array.
+   */
+  abstract enumeratePlaybackDevices(): AudioDeviceInfo[];
 
-/* api_iaudiodevicemanager_enumeraterecordingdevices */
-abstract enumerateRecordingDevices(): AudioDeviceInfo[];
+  /**
+   * Enumerates the audio capture devices.
+   *
+   * @returns
+   * Success: An AudioDeviceInfo array, which includes all the audio capture devices.Failure: An empty array.
+   */
+  abstract enumerateRecordingDevices(): AudioDeviceInfo[];
 
-/* api_iaudiodevicemanager_setplaybackdevice */
-abstract setPlaybackDevice(deviceId: string): number;
+  /**
+   * Sets the audio playback device.
+   *
+   * @param deviceId The ID of the specified audio playback device. You can get the device ID by calling enumeratePlaybackDevices . Connecting or disconnecting the audio device does not change the value of deviceId.The maximum length is MaxDeviceIdLengthType .
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setPlaybackDevice(deviceId: string): number;
 
-/* api_iaudiodevicemanager_getplaybackdevice */
-abstract getPlaybackDevice(): string;
+  /**
+   * Retrieves the audio playback device associated with the device ID.
+   *
+   * @returns
+   * The current audio playback device.
+   */
+  abstract getPlaybackDevice(): string;
 
-/* api_iaudiodevicemanager_getplaybackdeviceinfo */
-abstract getPlaybackDeviceInfo(): AudioDeviceInfo;
+  /**
+   * Retrieves the audio playback device associated with the device ID.
+   *
+   * @returns
+   * An AudioDeviceInfo object, which contains the ID and device name of the audio devices.
+   */
+  abstract getPlaybackDeviceInfo(): AudioDeviceInfo;
 
-/* api_iaudiodevicemanager_setplaybackdevicevolume */
-abstract setPlaybackDeviceVolume(volume: number): number;
+  /**
+   * Sets the volume of the audio capture device.
+   * This method applies to Windows only.
+   *
+   * @param volume The volume of the audio recording device. The value ranges between 0 (lowest volume) and 255 (highest volume). 0 means no sound, 255 means maximum volume.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setPlaybackDeviceVolume(volume: number): number;
 
-/* api_iaudiodevicemanager_getplaybackdevicevolume */
-abstract getPlaybackDeviceVolume(): number;
+  /**
+   * @ignore
+   */
+  abstract getPlaybackDeviceVolume(): number;
 
-/* api_iaudiodevicemanager_setrecordingdevice */
-abstract setRecordingDevice(deviceId: string): number;
+  /**
+   * Sets the audio capture device.
+   *
+   * @param deviceId The ID of the audio capture device. You can get the Device ID by calling enumerateRecordingDevices . Connecting or disconnecting the audio device does not change the value of deviceId.The maximum length is MaxDeviceIdLengthType .
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setRecordingDevice(deviceId: string): number;
 
-/* api_iaudiodevicemanager_getrecordingdevice */
-abstract getRecordingDevice(): string;
+  /**
+   * Gets the current audio recording device.
+   *
+   * @returns
+   * The current audio recording device.
+   */
+  abstract getRecordingDevice(): string;
 
-/* api_iaudiodevicemanager_getrecordingdeviceinfo */
-abstract getRecordingDeviceInfo(): AudioDeviceInfo;
+  /**
+   * Retrieves the volume of the audio recording device.
+   *
+   * @returns
+   * An AudioDeviceInfo object, which includes the device ID and device name.
+   */
+  abstract getRecordingDeviceInfo(): AudioDeviceInfo;
 
-/* api_iaudiodevicemanager_setrecordingdevicevolume */
-abstract setRecordingDeviceVolume(volume: number): number;
+  /**
+   * Sets the volume of the audio capture device.
+   * This method applies to Windows only.
+   *
+   * @param volume The volume of the audio recording device. The value ranges between 0 (lowest volume) and 255 (highest volume). 0 means no sound, 255 means maximum volume.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setRecordingDeviceVolume(volume: number): number;
 
-/* api_iaudiodevicemanager_getrecordingdevicevolume */
-abstract getRecordingDeviceVolume(): number;
+  /**
+   * @ignore
+   */
+  abstract getRecordingDeviceVolume(): number;
 
-/* api_iaudiodevicemanager_setloopbackdevice */
-abstract setLoopbackDevice(deviceId: string): number;
+  /**
+   * Sets the loopback device.
+   * The SDK uses the current playback device as the loopback device by default. If you want to specify another audio device as the loopback device, call this method, and set deviceId to the loopback device you want to specify.This method applies to Windows only.The scenarios where this method is applicable are as follows:Use app A to play music through a Bluetooth headset; when using app B for a video conference, play through the speakers.If the loopback device is set as the Bluetooth headset, the SDK publishes the music in app A to the remote end.If the loopback device is set as the speaker, the SDK does not publish the music in app A to the remote end.If you set the loopback device as the Bluetooth headset, and then use a wired headset to play the music in app A, you need to call this method again, set the loopback device as the wired headset, and the SDK continues to publish the music in app A to remote end.
+   *
+   * @param deviceId Specifies the loopback device of the SDK. You can get the device ID by calling enumeratePlaybackDevices . Connecting or disconnecting the audio device does not change the value of deviceId.The maximum length is MaxDeviceIdLengthType .
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setLoopbackDevice(deviceId: string): number;
 
-/* api_iaudiodevicemanager_getloopbackdevice */
-abstract getLoopbackDevice(): string;
+  /**
+   * Gets the current loopback device.
+   * This method applies to Windows only.
+   *
+   * @returns
+   * The ID of the current loopback device.
+   */
+  abstract getLoopbackDevice(): string;
 
-/* api_iaudiodevicemanager_setplaybackdevicemute */
-abstract setPlaybackDeviceMute(mute: boolean): number;
+  /**
+   * Stops or resumes subscribing to the audio streams of all remote users.
+   * After successfully calling this method, the local user stops or resumes subscribing to the audio streams of all remote users, including all subsequent users.Call this method after joinChannel [2/2] .When using the spatial audio effect, if you need to set whether to stop subscribing to the audio streams of all remote users, Agora recommends calling this method instead of the muteAllRemoteAudioStreams method in IRtcEngine .After calling this method, you need to call updateSelfPosition and updateRemotePosition to update the spatial location of the local user and the remote user; otherwise, the settings in this method do not take effect.
+   *
+   * @param mute Whether to stop subscribing to the audio streams of all remote users:true: Stop subscribing to the audio streams of all remote users.false: Subscribe to the audio streams of all remote users.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setPlaybackDeviceMute(mute: boolean): number;
 
-/* api_iaudiodevicemanager_getplaybackdevicemute */
-abstract getPlaybackDeviceMute(): boolean;
+  /**
+   * @ignore
+   */
+  abstract getPlaybackDeviceMute(): boolean;
 
-/* api_iaudiodevicemanager_setrecordingdevicemute */
-abstract setRecordingDeviceMute(mute: boolean): number;
+  /**
+   * Stops or resumes subscribing to the audio streams of all remote users.
+   * After successfully calling this method, the local user stops or resumes subscribing to the audio streams of all remote users, including all subsequent users.Call this method after joinChannel [2/2] .When using the spatial audio effect, if you need to set whether to stop subscribing to the audio streams of all remote users, Agora recommends calling this method instead of the muteAllRemoteAudioStreams method in IRtcEngine .After calling this method, you need to call updateSelfPosition and updateRemotePosition to update the spatial location of the local user and the remote user; otherwise, the settings in this method do not take effect.
+   *
+   * @param mute Whether to stop subscribing to the audio streams of all remote users:true: Stop subscribing to the audio streams of all remote users.false: Subscribe to the audio streams of all remote users.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract setRecordingDeviceMute(mute: boolean): number;
 
-/* api_iaudiodevicemanager_getrecordingdevicemute */
-abstract getRecordingDeviceMute(): boolean;
+  /**
+   * @ignore
+   */
+  abstract getRecordingDeviceMute(): boolean;
 
-/* api_iaudiodevicemanager_startplaybackdevicetest */
-abstract startPlaybackDeviceTest(testAudioFilePath: string): number;
+  /**
+   * Starts the audio playback device test.
+   * This method tests whether the audio playback device works properly. Once a user starts the test, the SDK plays an audio file specified by the user. If the user can hear the audio, the playback device works properly.After calling this method, the SDK triggers the onAudioVolumeIndication callback every 100 ms, reporting uid = 1 and the volume information of the playback device.Ensure that you call this method before joining a channel.
+   *
+   * @param testAudioFilePath The path of the audio file. The data format is string in UTF-8.Supported file formats: wav, mp3, m4a, and aac.Supported file sample rates: 8000, 16000, 32000, 44100, and 48000 Hz.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract startPlaybackDeviceTest(testAudioFilePath: string): number;
 
-/* api_iaudiodevicemanager_stopplaybackdevicetest */
-abstract stopPlaybackDeviceTest(): number;
+  /**
+   * Stops the audio playback device test.
+   * This method stops the audio playback device test. You must call this method to stop the test after calling the startPlaybackDeviceTest method.Ensure that you call this method before joining a channel.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract stopPlaybackDeviceTest(): number;
 
-/* api_iaudiodevicemanager_startrecordingdevicetest */
-abstract startRecordingDeviceTest(indicationInterval: number): number;
+  /**
+   * Starts the audio capture device test.
+   * This method tests whether the audio capture device works properly. After calling this method, the SDK triggers the onAudioVolumeIndication callback at the time interval set in this method, which reports uid = 0 and the volume information of the capturing device.Ensure that you call this method before joining a channel.
+   *
+   * @param indicationInterval The time interval (ms) at which the SDK triggers the onAudioVolumeIndication callback. Agora recommends setting a value greater than 200 ms. This value must not be less than 10 ms; otherwise, you can not receive the onAudioVolumeIndication callback.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract startRecordingDeviceTest(indicationInterval: number): number;
 
-/* api_iaudiodevicemanager_stoprecordingdevicetest */
-abstract stopRecordingDeviceTest(): number;
+  /**
+   * Stops the audio capture device test.
+   * This method stops the audio capture device test. You must call this method to stop the test after calling the startRecordingDeviceTest method.Ensure that you call this method before joining a channel.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract stopRecordingDeviceTest(): number;
 
-/* api_iaudiodevicemanager_startaudiodeviceloopbacktest */
-abstract startAudioDeviceLoopbackTest(indicationInterval: number): number;
+  /**
+   * Starts an audio device loopback test.
+   * This method tests whether the local audio capture device and playback device are working properly. After starting the test, the audio capture device records the local audio, and the audio playback device plays the captured audio. The SDK triggers two independent onAudioVolumeIndication callbacks at the time interval set in this method, which reports the volume information of the capture device (uid = 0) and the volume information of the playback device (uid = 1) respectively.Ensure that you call this method before joining a channel.This method tests local audio devices and does not report the network conditions.
+   *
+   * @param indicationInterval The time interval (ms) at which the SDK triggers the onAudioVolumeIndication callback. Agora recommends setting a value greater than 200 ms. This value must not be less than 10 ms; otherwise, you can not receive the onAudioVolumeIndication callback.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract startAudioDeviceLoopbackTest(indicationInterval: number): number;
 
-/* api_iaudiodevicemanager_stopaudiodeviceloopbacktest */
-abstract stopAudioDeviceLoopbackTest(): number;
+  /**
+   * Stops the audio device loopback test.
+   * Ensure that you call this method before joining a channel.Ensure that you call this method to stop the loopback test after calling the startAudioDeviceLoopbackTest method.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract stopAudioDeviceLoopbackTest(): number;
 
-/* api_iaudiodevicemanager_followsystemplaybackdevice */
-abstract followSystemPlaybackDevice(enable: boolean): number;
+  /**
+   * Sets the audio playback device used by the SDK to follow the system default audio playback device.
+   *
+   * @param enable Whether to follow the system default audio playback device:true: Follow the system default audio playback device. The SDK immediately switches the audio playback device when the system default audio playback device changes.false: Do not follow the system default audio playback device. The SDK switches the audio playback device to the system default audio playback device only when the currently used audio playback device is disconnected.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract followSystemPlaybackDevice(enable: boolean): number;
 
-/* api_iaudiodevicemanager_followsystemrecordingdevice */
-abstract followSystemRecordingDevice(enable: boolean): number;
+  /**
+   * Sets the audio recording device used by the SDK to follow the system default audio recording device.
+   *
+   * @param enable Whether to follow the system default audio recording device:true: Follow the system default audio playback device. The SDK immediately switches the audio recording device when the system default audio recording device changes.false: Do not follow the system default audio playback device. The SDK switches the audio recording device to the system default audio recording device only when the currently used audio recording device is disconnected.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract followSystemRecordingDevice(enable: boolean): number;
 
-/* api_iaudiodevicemanager_followsystemloopbackdevice */
-abstract followSystemLoopbackDevice(enable: boolean): number;
+  /**
+   * Sets whether the loopback device follows the system default playback device.
+   * This method applies to Windows only.
+   *
+   * @param enable Whether to follow the system default audio playback device:true: Follow the system default audio playback device. When the default playback device of the system is changed, the SDK immediately switches to the loopback device.false: Do not follow the system default audio playback device. The SDK switches the audio loopback device to the system default audio playback device only when the current audio playback device is disconnected.
+   *
+   * @returns
+   * 0: Success.< 0: Failure.
+   */
+  abstract followSystemLoopbackDevice(enable: boolean): number;
 
-/* api_iaudiodevicemanager_release */
-abstract release(): void;
+  /**
+   * Releases all the resources occupied by the IAudioDeviceManager object.
+   */
+  abstract release(): void;
 
-/* api_iaudiodevicemanager_getplaybackdefaultdevice */
-abstract getPlaybackDefaultDevice(): AudioDeviceInfo;
+  /**
+   * Gets the default audio playback device.
+   * This method is for Windows and macOS only.
+   *
+   * @returns
+   * The details about the default audio playback device. See AudioDeviceInfo .
+   */
+  abstract getPlaybackDefaultDevice(): AudioDeviceInfo;
 
-/* api_iaudiodevicemanager_getrecordingdefaultdevice */
-abstract getRecordingDefaultDevice(): AudioDeviceInfo;
+  /**
+   * Gets the default audio capture device.
+   * This method is for Windows and macOS only.
+   *
+   * @returns
+   * The details about the default audio capture device. See AudioDeviceInfo .
+   */
+  abstract getRecordingDefaultDevice(): AudioDeviceInfo;
 }
