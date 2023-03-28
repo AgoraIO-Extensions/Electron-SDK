cbdced4161b3d1d973d05b50f5d4222c275139fa
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2023-03-28 17:27:13 +0900

    refactor: migrate StringUid Example from class to hook (#959)
    refactor: migrate JoinMultipleChannel Example from class to hook
    refactor: migrate DeviceManager Example from class to hook
    refactor: migrate JoinChannelAudio Example from class to hook
    refactor: migrate VirtualBackground Example from class to hook
    feat: add hook useInitRtcEngine
    docs: example readme updated

--- example/src/renderer/examples/hooks/index.tsx
+++ example/src/renderer/examples/hooks/index.tsx
@@ -1,10 +1,40 @@
 import JoinChannelVideo from './JoinChannelVideo/JoinChannelVideo';
+import StringUid from './StringUid/StringUid';
+import JoinChannelAudio from './JoinChannelAudio/JoinChannelAudio';
+import JoinMultipleChannel from './JoinMultipleChannel/JoinMultipleChannel';
+import VirtualBackground from './VirtualBackground/VirtualBackground';
+import DeviceManager from './DeviceManager/DeviceManager';
 
 const hooksRoutes = [
   {
     path: '/JoinChannelVideoWithHook',
     component: JoinChannelVideo,
     title: 'JoinChannelVideoWithHook',
+  },
+  {
+    path: '/JoinChannelAudioWithHook',
+    component: JoinChannelAudio,
+    title: 'JoinChannelAudioWithHook',
+  },
+  {
+    path: '/StringUidHook',
+    component: StringUid,
+    title: 'StringUidHook',
+  },
+  {
+    path: '/JoinMultipleChannelHook',
+    component: JoinMultipleChannel,
+    title: 'JoinMultipleChannelHook',
+  },
+  {
+    path: '/VirtualBackgroundHook',
+    component: VirtualBackground,
+    title: 'VirtualBackgroundHook',
+  },
+  {
+    path: '/DeviceManagerHook',
+    component: DeviceManager,
+    title: 'DeviceManagerHook',
   },
 ];
 
