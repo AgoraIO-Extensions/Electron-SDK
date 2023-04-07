b1221432e0db632a59f376024ad20c0c38aaa49a
Arthas <15215604969@163.com>
2023-04-07 15:28:59 +0800

    [AUTO] Generate code by terra (#974)
    Co-authored-by: LichKing-2234 <LichKing-2234@users.noreply.github.com>

--- ts/Renderer/RendererManager.ts
+++ ts/Renderer/RendererManager.ts
@@ -72,10 +72,10 @@ export class RendererManager {
   }
 
   /**
-   * Sets dual-stream mode on the sender side.
-   * The SDK enables the low-quality video stream auto mode on the sender by default, which is equivalent to calling this method and setting the mode to AutoSimulcastStream. If you want to modify this behavior, you can call this method and modify the mode to DisableSimulcastStream(never send low-quality video streams) or EnableSimulcastStream (always send low-quality video streams).The difference and connection between this method and enableDualStreamMode [1/3] is as follows:When calling this method and setting mode to DisableSimulcastStream, it has the same effect as enableDualStreamMode [1/3](false).When calling this method and setting mode to EnableSimulcastStream, it has the same effect as enableDualStreamMode [1/3](true).Both methods can be called before and after joining a channel. If they are used at the same time, the settings in the method called later shall prevail.
+   * Sets the channel mode of the current audio file.
+   * In a stereo music file, the left and right channels can store different audio data. According to your needs, you can set the channel mode to original mode, left channel mode, right channel mode, or mixed channel mode. For example, in the KTV scenario, the left channel of the music file stores the musical accompaniment, and the right channel stores the singing voice. If you only need to listen to the accompaniment, call this method to set the channel mode of the music file to left channel mode; if you need to listen to the accompaniment and the singing voice at the same time, call this method to set the channel mode to mixed channel mode.Call this method after calling open .This method only applies to stereo audio files.
    *
-   * @param mode The mode in which the video stream is sent. See SimulcastStreamMode .
+   * @param mode The channel mode. See AudioDualMonoMode .
    *
    * @returns
    * 0: Success.< 0: Failure.
@@ -310,10 +310,10 @@ export class RendererManager {
   }
 
   /**
-   * Enables interoperability with the Agora Web SDK (applicable only in the live streaming scenarios).
-   * Deprecated:The SDK automatically enables interoperability with the Web SDK, so you no longer need to call this method.You can call this method to enable or disable interoperability with the Agora Web SDK. If a channel has Web SDK users, ensure that you call this method, or the video of the Native user will be a black screen for the Web user.This method is only applicable in live streaming scenarios, and interoperability is enabled by default in communication scenarios.
+   * Enables/Disables the local video capture.
+   * This method disables or re-enables the local video capture, and does not affect receiving the remote video stream.After calling enableVideo , the local video capture is enabled by default. You can call enableLocalVideo (false) to disable the local video capture. If you want to re-enable the local video capture, call enableLocalVideo(true).After the local video capturer is successfully disabled or re-enabled, the SDK triggers the onRemoteVideoStateChanged callback on the remote client.You can call this method either before or after joining a channel.This method enables the internal engine and is valid after leaving the channel.
    *
-   * @param enabled Whether to enable interoperability:true: Enable interoperability.false: (Default) Disable interoperability.
+   * @param enabled Whether to enable the local video capture.true: (Default) Enable the local video capture.false: Disable the local video capture. Once the local video is disabled, the remote users cannot receive the video stream of the local user, while the local user can still receive the video streams of remote users. When set to false, this method does not require a local camera.
    *
    * @returns
    * 0: Success.< 0: Failure.
@@ -537,13 +537,7 @@ export class RendererManager {
   }
 
   /**
-   * Sets the video encoder configuration.
-   * Sets the encoder configuration for the local video.You can call this method either before or after joining a channel. If the user does not need to reset the video encoding properties after joining the channel, Agora recommends calling this method before enableVideo to reduce the time to render the first video frame.
-   *
-   * @param config Video profile. See VideoEncoderConfiguration .
-   *
-   * @returns
-   * 0: Success.< 0: Failure.
+   * @ignore
    */
   private ensureRendererConfig(config: VideoFrameCacheConfig):
     | Map<
