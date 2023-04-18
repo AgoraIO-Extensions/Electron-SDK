237cf871da782e5fc49f53aa551c9b22b1c6e17f
HUI <luxuhui@agora.io>
2023-04-18 17:38:09 +0800

    build: optimize buildJS.js

--- scripts/buildJS.js
+++ scripts/buildJS.js
@@ -7,8 +7,7 @@ const buildJS = async (cb) => {
   logger.info('Build js from typescript');
   await cleanJSDir();
   await cleanTypesDir();
-  await exec('tsc', { silent: false });
-  await exec('tsc -p dtsconfig.json', { silent: false });
+  await exec('tsc -p tsconfig.build.json', { silent: false });
   cb();
 };
 
