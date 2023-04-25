5a7aa319e3082eaa1153472d23621c106408cd68
HUI <luxuhui@agora.io>
2023-04-25 18:33:30 +0800

    fix: ts compile error
    Former-commit-id: d0c0f7f7172749d48d16bb746b65c326299d8945

--- ts/Api/index.ts
+++ ts/Api/index.ts
@@ -1267,7 +1267,7 @@ class AgoraRtcEngine extends EventEmitter {
         if (channelStreams.size === 0) {
           this.streams.delete(channelId || '');
         }
-      } catch (err) {
+      } catch (err: any) {
         onFailure && onFailure(err);
       }
     }
@@ -1300,7 +1300,7 @@ class AgoraRtcEngine extends EventEmitter {
         if (channelStreams.size === 0) {
           this.streams.delete(channelId || '');
         }
-      } catch (err) {
+      } catch (err: any) {
         exception = err;
         console.error(`${err.stack}`);
       }
