efeada7c2d27a8f8a393b861b27a197e48d021e4
HUI <luxuhui@agora.io>
2023-05-06 13:23:14 +0800

    chore: optimize

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,19 +2,32 @@ const path = require('path');
 
 const fs = require('fs-extra');
 
+const logger = require('./logger');
+
 const destIrisSDKDir = path.join(__dirname, `../iris`);
 const buildDir = path.resolve(__dirname, '../build');
 
 exports.destIrisSDKDir = destIrisSDKDir;
 exports.buildDir = buildDir;
 
-exports.cleanIrisDir = async () => await fs.remove(destIrisSDKDir);
+exports.cleanIrisDir = async () => {
+  await fs.remove(destIrisSDKDir);
+  logger.info(`clean :${destIrisSDKDir}`);
+};
 
-exports.cleanBuildDir = async () =>
-  await fs.remove(`${path.resolve(__dirname, '../build')}`);
+exports.cleanBuildDir = async () => {
+  await fs.remove(buildDir);
+  logger.info(`clean :${buildDir}`);
+};
 
-exports.cleanJSDir = async () =>
-  await fs.remove(`${path.resolve(__dirname, '../js')}`);
+exports.cleanJSDir = async () => {
+  const dir = path.resolve(__dirname, '../js');
+  await fs.remove(dir);
+  logger.info(`clean :${dir}`);
+};
 
-exports.cleanTypesDir = async () =>
-  await fs.remove(`${path.resolve(__dirname, '../types')}`);
+exports.cleanTypesDir = async () => {
+  const dir = path.resolve(__dirname, '../types');
+  await fs.remove(dir);
+  logger.info(`clean :${dir}`);
+};
