7f3778754e6d88b6e554bebfd2b8cd6f6d0e5c75
HUI <luxuhui@agora.io>
2023-05-06 15:09:11 +0800

    chore: optimize

--- scripts/synclib.js
+++ scripts/synclib.js
@@ -18,8 +18,9 @@ const downloadSDK = async ({ preHook, postHook, sdkURL, destDir }) => {
     strip: 1,
     extract: true,
     filter: (file) => {
+      logger.info(JSON.stringify({ ...file, data: file.data.length }));
       return (
-        file.type === 'file' &&
+        file.type !== 'directory' &&
         !file.path.endsWith(path.sep) &&
         file.data.length !== 0
       );
