b66a2b0eb43ce0c3bfd62a757cb622fc7682df83
gxz <guoxianzhe@agora.io>
2023-07-24 21:42:09 +0800

    chore: change api name to align iris

--- ts/Private/impl/IAgoraRtcEngineImpl.ts
+++ ts/Private/impl/IAgoraRtcEngineImpl.ts
@@ -1252,7 +1252,7 @@ export class IRtcEngineImpl implements IRtcEngine {
     channelId: string,
     userAccount: string
   ): string {
-    return 'RtcEngine_preloadChannelWithUserAccount';
+    return 'RtcEngine_preloadChannel2';
   }
 
   updatePreloadChannelToken(token: string): number {
