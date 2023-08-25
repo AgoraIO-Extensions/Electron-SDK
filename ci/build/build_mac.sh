59459c50a38dd784402d3bc3dba7426325b28013
HUI <luxuhui@agora.io>
2023-08-25 16:33:44 +0800

    ci: fix

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -88,10 +88,8 @@ echo example_electron_version: $example_electron_version
 echo example_sdk_mode: $example_sdk_mode
 echo package_version: $package_version
 
-export NVM_DIR="$HOME/.nvm"
-source "${NVM_DIR}/nvm.sh" --install
-
 export https_proxy=http://10.80.1.174:1080 http_proxy=http://10.80.1.174:1080 all_proxy=http://10.80.1.174:1080
+source ~/.nvm/nvm.sh --install
 nvm ls-remote 14.17.3
 nvm install 14.17.3
 nvm use 14.17.3
