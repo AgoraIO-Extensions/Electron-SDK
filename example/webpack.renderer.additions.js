bd9f945cdd07687aa48e187a30ff2e131aead58f
HUI <luxuhui@agora.io>
2023-09-22 10:57:14 +0800

    [WIP]

--- example/webpack.renderer.additions.js
+++ example/webpack.renderer.additions.js
@@ -21,6 +21,18 @@ const fixModuleRules = (config) => {
         },
       ],
     },
+    {
+      // ...
+      test: /.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    },
     // {
     //   test: /\.[jt]sx?$/,
     //   exclude: /node_modules/,
