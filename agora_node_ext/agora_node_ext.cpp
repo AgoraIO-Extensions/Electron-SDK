22cd61f52c904bf9580129ae723f3a67bff489ca
sylar <huangpeilin@agora.io>
2023-10-18 18:16:07 +0800

    fix: add macro to fix compile issues for mac

--- agora_node_ext/agora_node_ext.cpp
+++ agora_node_ext/agora_node_ext.cpp
@@ -10,23 +10,29 @@
 
 #include "agora_node_ext.h"
 #include "agora_rtc_engine.h"
+#if defined(_WIN32)
 #include "dump.h"
+#endif
 
 using agora::rtc::NodeRtcEngine;
 using v8::Object;
 
+#if defined(_WIN32)
 void miniDmpSenderCallback(UINT nCode, LPVOID lVal1, LPVOID lVal2) {
   LOG_ERROR(
       "Crash happened, exception code : 0x%08x val0: 0x%08x val1 0x%08x\n",
       nCode, (std::uintptr_t)lVal1, (std::uintptr_t)lVal2);
 }
+#endif
 
 /**
  * Initialize NODEJS ADDON
  */
 void InitExt(Local<Object> module) {
   LOG_ENTER;
+#if defined(_WIN32)
   initializeDump(L"3.6.1.19", miniDmpSenderCallback);
+#endif
   NodeRtcEngine::Init(module);
   LOG_LEAVE;
 }
