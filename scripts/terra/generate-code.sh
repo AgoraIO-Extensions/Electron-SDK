40a2ac4b4e516660d1c0b97bd126de8f4c7fb8b8
gxz <guoxianzhe@agora.io>
2023-11-28 11:52:00 +0800

    chore: ci

--- scripts/terra/generate-code.sh
+++ scripts/terra/generate-code.sh
@@ -6,7 +6,7 @@ MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 
 npm exec terra -- run \
-    --config ${PROJECT_ROOT}/scripts/terra/terra_config_main.yaml  \
+    --config ${PROJECT_ROOT}/scripts/terra/code_config.yaml  \
     --output-dir=${PROJECT_ROOT}/ts/Private
 
 cd ${PROJECT_ROOT}
