4541b89ba25ee20cd5717194c027ed304d253b2c
gxz <guoxianzhe@agora.io>
2024-01-30 14:06:40 +0800

    chore: optimize

--- scripts/terra/generate-code.sh
+++ scripts/terra/generate-code.sh
@@ -5,13 +5,6 @@ set -x
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/../..)
 
-# treated as a completely separate project (not even a workspace), create an empty yarn.lock file in it.
-touch yarn.lock
-rm -rf node_modules
-rm -rf .terra
-yarn install
-rm yarn.lock
-
 npm exec terra -- run \
     --config ${PROJECT_ROOT}/scripts/terra/config/types_config.yaml  \
     --output-dir=${PROJECT_ROOT}/ts/Private
