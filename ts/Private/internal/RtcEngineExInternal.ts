87843c04d0d291a4993539f4c96cb6408cf2ea3a
gxz <guoxianzhe@agora.io>
2024-03-14 11:37:54 +0800

    feat: allow unregister event handler by specific eventHandler

--- ts/Private/internal/RtcEngineExInternal.ts
+++ ts/Private/internal/RtcEngineExInternal.ts
@@ -233,6 +233,8 @@ export class RtcEngineExInternal extends IRtcEngineExImpl {
   }
 
   override registerEventHandler(eventHandler: IRtcEngineEventHandler): boolean {
+    // only call iris when no event handler registered
+    let callIris = RtcEngineExInternal._event_handlers.length === 0;
     if (
       !RtcEngineExInternal._event_handlers.find(
         (value) => value === eventHandler
@@ -240,7 +242,7 @@ export class RtcEngineExInternal extends IRtcEngineExImpl {
     ) {
       RtcEngineExInternal._event_handlers.push(eventHandler);
     }
-    return super.registerEventHandler(eventHandler);
+    return callIris ? super.registerEventHandler(eventHandler) : true;
   }
 
   override unregisterEventHandler(
@@ -250,7 +252,9 @@ export class RtcEngineExInternal extends IRtcEngineExImpl {
       RtcEngineExInternal._event_handlers.filter(
         (value) => value !== eventHandler
       );
-    return super.unregisterEventHandler(eventHandler);
+    // only call iris when no event handler registered
+    let callIris = RtcEngineExInternal._event_handlers.length === 0;
+    return callIris ? super.unregisterEventHandler(eventHandler) : true;
   }
 
   override createMediaPlayer(): IMediaPlayer {
