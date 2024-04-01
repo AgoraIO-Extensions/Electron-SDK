d7bd5a97025ff0955fc9710844b652b2baa2d1c8
gxz <guoxianzhe@agora.io>
2024-04-01 17:45:21 +0800

    chore: optimize

---
+++ example/src/renderer/examples/basic/VideoDecoder/VideoDecoder.tsx
@@ -0,0 +1,171 @@
+import {
+  ChannelProfileType,
+  ClientRoleType,
+  EncodedVideoFrameInfo,
+  IRtcEngineEventHandler,
+  IRtcEngineEx,
+  IVideoEncodedFrameObserver,
+  RtcConnection,
+  UserOfflineReasonType,
+  VideoStreamType,
+  WebCodecsDecoder,
+  createAgoraRtcEngine,
+} from 'agora-electron-sdk';
+import React, { ReactElement } from 'react';
+
+import {
+  BaseAudioComponentState,
+  BaseComponent,
+} from '../../../components/BaseComponent';
+import Config from '../../../config/agora.config';
+import { askMediaAccess } from '../../../utils/permissions';
+
+interface State extends BaseAudioComponentState {
+  fps: number;
+}
+
+const SCREEN_UID = 7;
+
+export default class VideoDecoder
+  extends BaseComponent<{}, State>
+  implements IRtcEngineEventHandler, IVideoEncodedFrameObserver
+{
+  // @ts-ignore
+  protected engine?: IRtcEngineEx;
+  private decoder?: WebCodecsDecoder;
+
+  protected createState(): State {
+    return {
+      appId: Config.appId,
+      fps: 0,
+      enableVideo: true,
+      channelId: Config.channelId,
+      token: Config.token,
+      uid: Config.uid,
+      joinChannelSuccess: false,
+      remoteUsers: [],
+      startPreview: false,
+    };
+  }
+
+  /**
+   * Step 1: initRtcEngine
+   */
+  protected async initRtcEngine() {
+    const { appId } = this.state;
+    if (!appId) {
+      this.error(`appId is invalid`);
+    }
+
+    this.engine = createAgoraRtcEngine() as IRtcEngineEx;
+    this.engine.initialize({
+      appId,
+      logConfig: { filePath: Config.logFilePath },
+      // Should use ChannelProfileLiveBroadcasting on most of cases
+      channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+    });
+    this.engine.registerEventHandler(this);
+    this.engine.getMediaEngine().registerVideoEncodedFrameObserver(this);
+
+    // Need granted the microphone and camera permission
+    await askMediaAccess(['microphone', 'camera', 'screen']);
+
+    // Need to enable video on this case
+    // If you only call `enableAudio`, only relay the audio stream to the target channel
+    this.engine.enableVideo();
+    // Start preview before joinChannel
+    // this.engine.startPreview();
+    this.setState({ startPreview: true });
+  }
+
+  /**
+   * Step 2: joinChannel
+   */
+  protected joinChannel() {
+    const { channelId, token, uid } = this.state;
+    if (!channelId) {
+      this.error('channelId is invalid');
+      return;
+    }
+    if (uid < 0) {
+      this.error('uid is invalid');
+      return;
+    }
+
+    // start joining channel
+    // 1. Users can only see each other after they join the
+    // same channel successfully using the same app id.
+    // 2. If app certificate is turned on at dashboard, token is needed
+    // when joining channel. The channel name and uid used to calculate
+    // the token has to match the ones used for channel join
+    this.engine?.joinChannel(token, channelId, uid, {
+      // Make myself as the broadcaster to send stream to remote
+      clientRoleType: ClientRoleType.ClientRoleBroadcaster,
+    });
+    this.decoder = new WebCodecsDecoder();
+    this.decoder.enableFps = true;
+  }
+
+  /**
+   * Step 4: leaveChannel
+   */
+  protected leaveChannel() {
+    this.engine?.leaveChannel();
+  }
+
+  /**
+   * Step 5: releaseRtcEngine
+   */
+  protected releaseRtcEngine() {
+    this.engine?.getMediaEngine().unregisterVideoEncodedFrameObserver(this);
+    this.engine?.unregisterEventHandler(this);
+    this.engine?.release();
+  }
+
+  onEncodedVideoFrameReceived(
+    uid: number,
+    imageBuffer: Uint8Array,
+    length: number,
+    videoEncodedFrameInfo: EncodedVideoFrameInfo
+  ) {
+    if (uid == SCREEN_UID) {
+      // start decode
+      this.decoder?.decodeFrame(
+        imageBuffer,
+        videoEncodedFrameInfo,
+        new Date().getTime()
+      );
+      this.setState({ fps: this.decoder?.getFps() || 0 });
+    }
+  }
+
+  onUserJoined(connection: RtcConnection, remoteUid: number, elapsed: number) {
+    this.engine?.setRemoteVideoSubscriptionOptions(remoteUid, {
+      type: VideoStreamType.VideoStreamHigh,
+      encodedFrameOnly: true,
+    });
+    super.onUserJoined(connection, remoteUid, elapsed);
+  }
+
+  onUserOffline(
+    connection: RtcConnection,
+    remoteUid: number,
+    reason: UserOfflineReasonType
+  ) {
+    if (remoteUid == SCREEN_UID) {
+      // stop decode
+      this.decoder?.release();
+    }
+    super.onUserOffline(connection, remoteUid, reason);
+  }
+
+  protected renderUsers(): ReactElement | undefined {
+    let { fps } = this.state;
+    return (
+      <>
+        <p>Current Fps: {fps}</p>
+        <canvas />
+      </>
+    );
+  }
+}
---
+++ ts/Decoder/WebCodecsDecoder.ts
@@ -0,0 +1,151 @@
+import { EncodedVideoFrameInfo, VideoFrameType } from '../Private/AgoraBase';
+
+import { logDebug, logError } from '../Utils';
+
+import { WebGLRenderer } from './renderer_webgl';
+
+const frameTypeMapping = {
+  [VideoFrameType.VideoFrameTypeDeltaFrame]: 'delta',
+  [VideoFrameType.VideoFrameTypeKeyFrame]: 'key',
+};
+
+export class WebCodecsDecoder {
+  public enableFps = false;
+
+  // eslint-disable-next-line auto-import/auto-import
+  private _decoder: VideoDecoder;
+  private _startTime: number | null = null;
+  private renderer: any;
+  // eslint-disable-next-line auto-import/auto-import
+  private pendingFrame: VideoFrame | null = null;
+  private _frameCount = 0;
+
+  private _frame_ts: number[] = [];
+  private _base_ts = 0;
+  private _base_ts_ntp = 1;
+  private _last_ts_ntp = 1;
+
+  constructor() {
+    this.renderer = new WebGLRenderer(
+      'webgl2',
+      document.querySelector('canvas')!.transferControlToOffscreen()
+    );
+    // eslint-disable-next-line auto-import/auto-import
+    this._decoder = new VideoDecoder({
+      // @ts-ignore
+      output: this._output.bind(this),
+      error: this._error.bind(this),
+    });
+    this.decoder!.configure({
+      codec: 'hvc1.1.2.H153.90',
+      codedWidth: 3840,
+      codedHeight: 2160,
+    });
+  }
+
+  // eslint-disable-next-line auto-import/auto-import
+  getDecoder(): VideoDecoder {
+    return this._decoder;
+  }
+
+  // eslint-disable-next-line auto-import/auto-import
+  _output(frame: VideoFrame) {
+    this.getFps();
+    // Schedule the frame to be rendered.
+    this._renderFrame(frame);
+  }
+
+  _error(e: any) {
+    console.error('Decoder error:', e);
+  }
+
+  get decoder() {
+    return this._decoder;
+  }
+
+  getFps(): number {
+    let fps = 0;
+    if (!this.enableFps) {
+      return fps;
+    }
+    // Update statistics.
+    if (this._startTime == null) {
+      this._startTime = performance.now();
+    } else {
+      const elapsed = (performance.now() - this._startTime) / 1000;
+      fps = ++this._frameCount / elapsed;
+      logDebug('render', `${fps.toFixed(0)} fps`);
+    }
+    return fps;
+  }
+
+  // eslint-disable-next-line auto-import/auto-import
+  _renderFrame(frame: VideoFrame) {
+    if (!this.pendingFrame) {
+      // Schedule rendering in the next animation frame.
+      // eslint-disable-next-line auto-import/auto-import
+      requestAnimationFrame(this.renderAnimationFrame.bind(this));
+    } else {
+      // Close the current pending frame before replacing it.
+      this.pendingFrame.close();
+    }
+    // Set or replace the pending frame.
+    this.pendingFrame = frame;
+  }
+
+  renderAnimationFrame() {
+    this.renderer.draw(this.pendingFrame);
+    this.pendingFrame = null;
+  }
+
+  // @ts-ignore
+  decodeFrame(
+    imageBuffer: Uint8Array,
+    frameInfo: EncodedVideoFrameInfo,
+    ts: number
+  ) {
+    if (!imageBuffer) {
+      logError('imageBuffer is empty, cannot decode frame');
+      return;
+    }
+    this._frame_ts.push(ts);
+    if (this._base_ts !== 0) {
+      if (ts > this._base_ts) {
+        this._last_ts_ntp =
+          this._base_ts_ntp + Math.floor(((ts - this._base_ts) * 1000) / 90);
+      } else {
+        this._base_ts = ts;
+        this._last_ts_ntp++;
+        this._base_ts_ntp = this._last_ts_ntp;
+      }
+    } else {
+      this._base_ts = ts;
+      this._last_ts_ntp = 1;
+    }
+    let frameType: string | undefined;
+    if (frameInfo.frameType !== undefined) {
+      // @ts-ignore
+      frameType = frameTypeMapping[frameInfo.frameType];
+    }
+    if (!frameType) {
+      logError('frameType is incorrect, cannot decode frame');
+      return;
+    }
+    this._decoder.decode(
+      // eslint-disable-next-line auto-import/auto-import
+      new EncodedVideoChunk({
+        data: imageBuffer,
+        timestamp: this._last_ts_ntp,
+        // @ts-ignore
+        type: frameType,
+        // @ts-ignore
+        transfer: [imageBuffer.buffer],
+      })
+    );
+  }
+
+  release() {
+    this._decoder.reset();
+    this._decoder.close();
+  }
+}
