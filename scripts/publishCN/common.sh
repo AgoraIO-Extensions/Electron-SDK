e5be1498d17ba6e9606b60e6e44ccfe4cc26ea3f
夏羊群 <xiayangqun@agora.io>
2024-12-19 16:26:13 +0800

    chore: sync.yml

---
+++ scripts/publishCN/common.sh
@@ -0,0 +1,19 @@
+# common.sh
+#!/bin/bash
+
+old_package_name='agora-electron-sdk'
+new_package_name='shengwang-electron-sdk'
+
+#./package.json
+old_name="\"name\": \"agora-electron-sdk\""
+new_name="\"name\": \"shengwang-electron-sdk\""
+
+old_description="\"description\": \"agora-electron-sdk\""
+new_description="\"description\": \"shengwang-electron-sdk\""
+
+old_yarn_link="yarn example link agora-electron-sdk"
+new_yarn_link="yarn example link shengwang-electron-sdk"
+
+# node_modules
+old_node_modules="node_modules/agora-electron-sdk"
+new_node_modules="node_modules/shengwang-electron-sdk"
