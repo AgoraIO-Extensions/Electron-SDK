86a469a878a5c72dde74ba41a9d4ec2e554c8a13
Arthas <luxuhui@agora.io>
2025-09-25 11:41:07 +0800

    fix: update proxy settings in Mac build script

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -88,7 +88,7 @@ echo example_electron_version: $example_electron_version
 echo example_sdk_mode: $example_sdk_mode
 echo package_version: $package_version
 
-export https_proxy=http://10.80.1.174:1080 http_proxy=http://10.80.1.174:1080 all_proxy=http://10.80.1.174:1080
+export https_proxy=http://10.15.67.1:18080 http_proxy=http://110.15.67.1:18080 all_proxy=http://10.15.67.1:18080
 source ~/.nvm/nvm.sh --install
 nvm ls-remote 14.17.3
 nvm install 14.17.3
