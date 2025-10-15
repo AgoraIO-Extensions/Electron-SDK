e2b5ebd855d396d4024934a13789efdedc0666d2
gxz <guoxianzhe@agora.io>
2025-10-15 19:25:33 +0800

    chore: change ci job

--- scripts/internal/dep.sh
+++ scripts/internal/dep.sh
@@ -11,50 +11,27 @@ if [ "$#" -lt 1 ]; then
 fi
 INPUT=$1
 
-MAC_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "macOS") | .cdn[]')
 IRIS_MAC_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "macOS") | .iris_cdn[]')
-WINDOWS_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Windows") | .cdn[]')
 IRIS_WINDOWS_DEPENDENCIES=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Windows") | .iris_cdn[]')
 DEP_VERSION=$(echo "$INPUT" | jq -r '.[] | select(.platform == "Windows") | .version')
-
-if [ -z "$MAC_DEPENDENCIES" ]; then
-  echo "No mac native dependencies need to change."
-else
-  for DEP in $MAC_DEPENDENCIES; do
-    sed 's|"native_sdk_mac": "\(.*\)"|"native_sdk_mac": "'"$DEP"'"|g' $PACKAGE_JSON_PATH > tmp
-    mv tmp package.json
-    break
-  done
-fi
 
 if [ -z "$IRIS_MAC_DEPENDENCIES" ]; then
   echo "No iris mac native dependencies need to change."
 else
   for DEP in $IRIS_MAC_DEPENDENCIES; do
-    if [[ "$DEP" == *Standalone* ]]; then
-      sed 's|"iris_sdk_mac": "\(.*\)"|"iris_sdk_mac": "'"$DEP"'"|g' $PACKAGE_JSON_PATH > tmp
-      mv tmp package.json
-      break
-    fi
+    sed 's|"iris_sdk_mac": "\(.*\)"|"iris_sdk_mac": "'"$DEP"'"|g' $PACKAGE_JSON_PATH > tmp
+    mv tmp package.json
+    break
   done
-fi
-
-if [ -z "$WINDOWS_DEPENDENCIES" ]; then
-  echo "No windows native dependencies need to change."
-else
-  sed 's|"native_sdk_win": "\(.*\)"|"native_sdk_win": "'"$WINDOWS_DEPENDENCIES"'"|g' $PACKAGE_JSON_PATH > tmp
-  mv tmp package.json
 fi
 
 if [ -z "$IRIS_WINDOWS_DEPENDENCIES" ]; then
   echo "No iris windows native dependencies need to change."
 else
   for DEP in $IRIS_WINDOWS_DEPENDENCIES; do
-    if [[ "$DEP" == *Standalone* ]]; then
-      sed 's|"iris_sdk_win": "\(.*\)"|"iris_sdk_win": "'"$DEP"'"|g' $PACKAGE_JSON_PATH > tmp
-      mv tmp package.json
-      break
-    fi
+    sed 's|"iris_sdk_win": "\(.*\)"|"iris_sdk_win": "'"$DEP"'"|g' $PACKAGE_JSON_PATH > tmp
+    mv tmp package.json
+    break
   done
 fi
 
