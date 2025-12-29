318ceee571027a1713fde968b2435b02a6bb5415
gxz <guoxianzhe@agora.io>
2025-12-29 16:29:43 +0800

    chore: update build_all_platforms.groovy

--- ci/build/build_all_platforms.groovy
+++ ci/build/build_all_platforms.groovy
@@ -2,6 +2,7 @@ properties([
     buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '7', numToKeepStr: '100')),
     parameters([
         string(name: 'electron_sdk_branch', defaultValue: '', description: 'Electron branch', trim: true),
+      	booleanParam(name: 'is_tag_fetch', defaultValue: true),
         string(name: 'network_path', defaultValue: '', description: '', trim: true),
         string(name: 'package_version', defaultValue: 'package_version', trim: true),
         string(name: 'example_sdk_mode', defaultValue: '', description: '', trim: true),
@@ -20,15 +21,19 @@ properties([
 ])
 
 timestamps {
+    build job: 'AD/Sync_Github', propagate: false, parameters: [
+          string(name: 'repos', value: 'electron-sdk'),
+      ], wait: true
     def commonBuildParams = [
         string(name: 'electron_sdk_branch', value: params.electron_sdk_branch),
+      	booleanParam(name:'is_tag_fetch', value: params.is_tag_fetch),
         string(name: 'network_path', value: params.network_path),
+      	string(name: 'package_version', value: params.package_version),
         string(name: 'example_sdk_mode', value: params.example_sdk_mode),
         string(name: 'example_electron_version', value: params.example_electron_version),
-        string(name: 'package_version', value: params.package_version),
         booleanParam(name: 'Package_Publish', value: true),
         booleanParam(name:'Clean_Clone', value: true),
-        booleanParam(name:'isBuildSdk', value: true),
+        booleanParam(name:'isBuildSdk', value: params.Upload_CDN ? true : (params.isBuildDemo ? params.example_sdk_mode == "1" : false)),
         booleanParam(name:'isBuildDemo', value: params.isBuildDemo),
         booleanParam(name: 'Upload_CDN', value: params.Upload_CDN)
     ]
