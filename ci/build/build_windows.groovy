f2fa8f1c7ab856f4ca2b93583c1c786dfee0064a
gxz <guoxianzhe@agora.io>
2026-04-16 09:40:19 +0800

    chore: optimize

--- ci/build/build_windows.groovy
+++ ci/build/build_windows.groovy
@@ -18,14 +18,6 @@ compileConfig = [
 ]
 
 def doBuild(buildVariables) {
-    gitCheckOut(getConfig(), params.repository, false, params.is_tag_fetch, [
-      "./electron-sdk": [
-          params.repository,
-          params.electron_sdk_branch,
-          '',
-          getConfig().CREDENTIAL_SSH
-      ]
-    ])
     type = params.Package_Publish ? "publish" : "non-publish"
     command = compileConfig.get(type).command
     preCommand = compileConfig.get(type).get("preCommand", "")
