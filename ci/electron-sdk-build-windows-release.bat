cd1f32aa71bcc3097428de34fb98c9a8dbd26d97
gxz <guoxianzhe@agora.io>
2026-05-15 11:34:33 +0800

    chore: optimize

--- ci/electron-sdk-build-windows-release.bat
+++ ci/electron-sdk-build-windows-release.bat
@@ -16,20 +16,6 @@ if %1 == x64 (
 )
 call npm config delete agora_electron_sdk_pre_built
 
-set count=0
-:loop
-set /a count=%count%+1
-
-C:\\SignatureTools\\signtool.exe sign /f "C:\\SignatureTools\\agora.pfx" /p "31169323" /t "http://timestamp.comodoca.com/authenticode" "build\\Release\\agora_node_ext.node"
-if %count% equ 5 goto endloop
-if %errorlevel% equ 1 (
-    sleep 15
-    goto loop
-)
-:endloop
-if %count% equ 5 (
-    echo "******sign error*******"
-    exit 1
-)
+echo Skip local signtool signing. Windows package signing is handled by Jenkins CodeSign after Artifactory upload.
 
 call npm run zipBuild
